17ada4fa4091a3794170e2cfef5c2c90a60662d0
David Shea <dshea@redhat.com>
2016-12-12 10:49:32 -0500

    Fix the recipe component display.
    Use the correct URL for the recipe.json data, and update the components
    list with the list part of the data instead of the whole object.

--- core/constants.js
+++ core/constants.js
@@ -8,7 +8,7 @@ const constants = {
   get_users_url: composer_api_host + '/users',
   get_recipes_url: composer_api_host + "/api/v0/recipe/list",
   // TODO get this using the regular recipe URL, rename "recipe_api_url" to something less dumb
-  get_recipe_url: composer_api_host + '/recipes',
+  get_recipe_url: composer_api_host + '/recipe',
   get_recipe_api_url: composer_api_host + "/api/v0/recipe/",
   get_components_url: composer_api_host + "/api/v0/module/list",
   get_comptypes_url: composer_api_host + "/api/v0/compose/types"
