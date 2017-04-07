5bd632f7da6fe3468aae360ae9d1e0c3456921a2
Jennifer Giardino <jgiardin@redhat.com>
2017-04-07 13:01:33 -0400

    Save updates - include version, display message
    Save was updated to include version number so that
    version is automatically incremented, then recipe details
    are reloaded to display new version number. Version number
    is now visible in the Edit Recipe header.
    
    Notifications were updated to include messages for save;
    messages were also refactored; close function was updated
    to accept either a message index or message label; notifications
    component was also updated to support all message types (e.g.
    warning, error, info).

--- data/NotificationsAPI.js
+++ data/NotificationsAPI.js
@@ -10,35 +10,93 @@ class NotificationsApi {
   }
 
   displayNotification(recipe, message) {
-    const notification = {};
-    if (message === 'creating') {
-      notification.type = 'process';
-      notification.message = <span><strong>{recipe}:</strong> Creating composition.</span>;
-      notification.action = <a href="#">Cancel</a>;
-      notification.dismiss = true;
-      const index = this.notifications.length;
-      setTimeout(() => {
-        this.closeNotification(index);
-        this.displayNotification(recipe, 'created');
-      }, 2500);
-      // setTimeout is only temporary, and included to simulate what will happen
-      // when the user creates a composition (i.e. display process message
-      // then success notification); this should be updated
-      // when composition creation is fully implemented
-    } else if (message === 'created') {
-      notification.type = 'success';
-      notification.message = <span><strong>{recipe}:</strong> Composition creation is complete.</span>;
-      // notification.action = <a href="#void">Download (.iso)</a>;
-      notification.kebab = [
-        <a href="#" id="cmpsr-bom-link">Export Recipe (.bom)</a>,
-      ];
-      notification.fade = true;
-    }
+    const notification = this.notificationMessage(recipe, message);
     this.notifications.push(notification);
   }
 
-  closeNotification(id) {
-    this.notifications.splice(id, 1);
+  closeNotification(id, label) {
+    // const index = id || (this.notifications.map(notification => { notification.label; }).indexOf(label));
+    let index = id;
+    if (index === undefined) {
+      for (let i = 0; i < this.notifications.length; i++) {
+        if (this.notifications[i].label === label) {
+          index = i;
+        }
+      }
+    }
+    if (index !== undefined) {
+      this.notifications.splice(index, 1);
+    }
+  }
+
+  notificationMessage(recipe, message) {
+    let notification = {};
+    switch (message) {
+      case 'creating': {
+        notification = {
+          type: 'process',
+          message: <span><strong>{recipe}:</strong> Creating composition.</span>,
+          action: <a href="#">Cancel</a>,
+          dismiss: true,
+        };
+        const index = this.notifications.length;
+        setTimeout(() => {
+          this.closeNotification(index);
+          this.displayNotification(recipe, 'created');
+        }, 2500);
+        // setTimeout is only temporary, and included to simulate what will happen
+        // when the user creates a composition (i.e. display process message
+        // then success notification); this should be updated
+        // when composition creation is fully implemented
+        break;
+      }
+      case 'created': {
+        notification = {
+          type: 'success',
+          message: <span><strong>{recipe}:</strong> Composition creation is complete.</span>,
+          // action: <a href="#void">Download (.iso)</a>,
+          // this link will need to be implemented when the build process
+          // is implemented; this function will need to be extended to handle
+          // defining this link
+          kebab: [
+            <a href="#" id="cmpsr-bom-link">Export Recipe (.bom)</a>,
+          ],
+          fade: true,
+        };
+        break;
+      }
+      case 'saving': {
+        notification = {
+          type: 'process',
+          label: 'saving',
+          message: <span><strong>{recipe}:</strong> Saving recipe.</span>,
+          dismiss: true,
+        };
+        break;
+      }
+      case 'saved': {
+        notification = {
+          type: 'success',
+          label: 'saved',
+          message: <span><strong>{recipe}:</strong> Recipe is saved.</span>,
+          dismiss: true,
+          fade: true,
+        };
+        break;
+      }
+      case 'saveFailed': {
+        notification = {
+          type: 'error',
+          message: <span><strong>{recipe}:</strong> Save failed.</span>,
+          dismiss: true,
+        };
+        break;
+      }
+      default: {
+        notification = {};
+      }
+    }
+    return notification;
   }
 
 }
