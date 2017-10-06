5b8c6f2c71c6e23363049cd305a00c5f0223bd11
Jennifer Giardino <jgiardin@redhat.com>
2017-10-06 15:10:31 -0400

    Update Recipes page empty state and non-functional actions
    Update Empty State message and include Create Recipe action
    to be more consistent with the patternfly pattern. Remove actions
    from the toolbar kebab that are not functional and have no plans
    to be implemented in the near future.

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -117,51 +117,11 @@ class RecipesPage extends React.Component {
                 </button>
               }
               </div>
-
               <div className="toolbar-pf-action-right">
                 <div className="form-group">
                   <button className="btn btn-default" type="button" data-toggle="modal" data-target="#cmpsr-modal-crt-recipe">
                     Create Recipe
                   </button>
-                  <div className="dropdown btn-group  dropdown-kebab-pf">
-                    <button
-                      className="btn btn-link dropdown-toggle"
-                      type="button"
-                      id="dropdownKebab"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <span className="fa fa-ellipsis-v" />
-                    </button>
-                    <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownKebab">
-                      <li><a>Import Recipe</a></li>
-                      <li role="separator" className="divider" />
-                      <li><a>Create Compositions</a></li>
-                      <li><a>Export Selected Recipes</a></li>
-                      <li className="hidden"><a>Archive Selected Recipes</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="form-group toolbar-pf-find">
-                  <button className="btn btn-link btn-find" type="button">
-                    <span className="fa fa-search" />
-                  </button>
-                  <div className="find-pf-dropdown-container">
-                    <input type="text" className="form-control" id="find" placeholder="Find By Keyword..." />
-                    <div className="find-pf-buttons">
-                      <span className="find-pf-nums">1 of 3</span>
-                      <button className="btn btn-link" type="button">
-                        <span className="fa fa-angle-up" />
-                      </button>
-                      <button className="btn btn-link" type="button">
-                        <span className="fa fa-angle-down" />
-                      </button>
-                      <button className="btn btn-link btn-find-close" type="button">
-                        <span className="pficon pficon-close" />
-                      </button>
-                    </div>
-                  </div>
                 </div>
               </div>
             </form>
@@ -169,17 +129,19 @@ class RecipesPage extends React.Component {
         </div>
       {recipes.length === 0 &&
         <EmptyState
-          title="Construct and maintain &ldquo;gold images&rdquo; across a variety of deployment platforms"
-          message="Create your first recipe with the &ldquo;Create Recipe&rdquo; button above."
+          title="No Recipes"
+          message={`Create a recipe to define the contents that will be included
+            in the images you create. Images can be produced in a variety of
+            output formats.`}
         >
-          <dl className="dl-horizontal">
-            <dt>Recipe</dt>
-            <dd>Lists the contents that will be included in the gold image
-            when it is generated.</dd>
-            <dt>Composition</dt>
-            <dd>The gold image itself, which can be produced in a
-            variety of output formats.</dd>
-          </dl>
+          <button
+            className="btn btn-primary btn-lg"
+            type="button"
+            data-toggle="modal"
+            data-target="#cmpsr-modal-crt-recipe"
+          >
+            Create Recipe
+          </button>
         </EmptyState>
       }
       {createComposition.compositionTypes !== undefined &&
