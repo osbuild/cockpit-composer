669da019ad912209423d6d790df763cb37919e8d
Brian C. Lane <bcl@redhat.com>
2018-04-02 14:04:09 -0700

    Switch to using the blueprints API instead of recipes.
    This requires an API server that has also made the change.

--- core/constants.js
+++ core/constants.js
@@ -4,17 +4,17 @@
 
 const constants = {
   // BDCS API paths
-  get_blueprints_list: '/api/v0/recipes/list',
-  get_blueprints_info: '/api/v0/recipes/info/',
-  get_blueprints_deps: '/api/v0/recipes/depsolve/',
+  get_blueprints_list: '/api/v0/blueprints/list',
+  get_blueprints_info: '/api/v0/blueprints/info/',
+  get_blueprints_deps: '/api/v0/blueprints/depsolve/',
   get_modules_list: '/api/v0/modules/list',
   get_projects_info: '/api/v0/projects/info/',
   get_projects_deps: '/api/v0/projects/depsolve/',
   get_modules_info: '/api/v0/modules/info/',
   get_image_types: '/api/v0/compose/types',
-  post_blueprints_new: '/api/v0/recipes/new',
-  post_blueprints_workspace: '/api/v0/recipes/workspace',
-  delete_blueprint: '/api/v0/recipes/delete/',
+  post_blueprints_new: '/api/v0/blueprints/new',
+  post_blueprints_workspace: '/api/v0/blueprints/workspace',
+  delete_blueprint: '/api/v0/blueprints/delete/',
 
 };
 
