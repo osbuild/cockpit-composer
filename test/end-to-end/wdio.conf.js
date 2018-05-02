a7bd0877b3105ab83df90f83d036c09c99b212c7
Alexander Todorov <atodorov@redhat.com>
2018-05-02 15:49:58 +0300

    Enable spec reporter for e2e tests

--- test/end-to-end/wdio.conf.js
+++ test/end-to-end/wdio.conf.js
@@ -123,7 +123,7 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: http://webdriver.io/guide/reporters/dot.html
-  // reporters: ['dot'],
+  reporters: ['dot', 'spec'],
   //
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
