0c91cdc7eff05d394f3ce2cbcea9bb6ad0c46404
Jennifer Giardino <jgiardin@redhat.com>
2018-07-02 09:19:34 -0400

    sets timeout on Loading state

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const Loading = () => (
-  <div className="cmpsr-loading">
-    <div className="spinner spinner-md"></div>
-    <p className="lead">Loading</p>
-  </div>
-);
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.enableLoadingState = this.enableLoadingState.bind(this);
+    this.state = {
+      displayLoadingState: false,
+    };
+    this.timer = setTimeout(this.enableLoadingState, 500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  enableLoadingState() {
+    this.setState({displayLoadingState: true});
+  }
+
+  render() {
+    const {displayLoadingState} = this.state;
+    if (!displayLoadingState) {
+      return null;
+    }
+    return (
+      <div className="cmpsr-loading">
+        <div className="spinner spinner-md"></div>
+        <p className="lead">Loading</p>
+      </div>
+    );
+  }
+}
 
 export default Loading;
