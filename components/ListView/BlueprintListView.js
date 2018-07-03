3ab90e395fff07a43c8823898cc26c7ed8edf535
Jennifer Giardino <jgiardin@redhat.com>
2018-07-03 09:22:12 -0400

    fixes bad merges during rebase

--- components/ListView/BlueprintListView.js
+++ components/ListView/BlueprintListView.js
@@ -9,11 +9,6 @@ class BlueprintListView extends React.PureComponent {
     super();
   }
 
-  handleCreateImage(blueprint) {
-    this.setState({ blueprint });
-    $('#cmpsr-modal-crt-image').modal('show');
-  }
-
   render() {
     const { blueprints } = this.props; // eslint-disable-line no-use-before-define
     return (
@@ -39,16 +34,24 @@ class BlueprintListView extends React.PureComponent {
                   <button
                     type="button"
                     className="btn btn-default"
-                    onClick={() => this.handleCreateImage(blueprint.name)}
+                    onClick={(e) => this.props.handleShowModalCreateImage(e, blueprint)}
                   >
                     Create Image
                   </button>
                 }
                 <div className="dropdown pull-right dropdown-kebab-pf">
-                  <button
-                    className="btn btn-default"
-                    onClick={(e) => this.props.handleShowModalCreateImage(e, blueprint)}
-                  >
+                   <button
+                     className="btn btn-link dropdown-toggle"
+                     type="button"
+                     id="dropdownKebabRight9"
+                     data-toggle="dropdown"
+                     aria-haspopup="true"
+                     aria-expanded="true"
+                   ><span className="fa fa-ellipsis-v"></span></button>
+                   <ul
+                     className="dropdown-menu dropdown-menu-right"
+                     aria-labelledby="dropdownKebabRight9"
+                   >
                     {(blueprint.modules.length === 0 && blueprint.packages.length === 0) &&
                       <li className="disabled"><a aria-disabled="true">Export</a></li>
                       ||
@@ -73,90 +76,71 @@ class BlueprintListView extends React.PureComponent {
                     <div
                       className="list-view-pf-additional-info-item list-view-pf-additional-info-item-stacked hidden"
                     >
-                      <li><a href="#" onClick={(e) => this.props.handleShowModalExport(e, blueprint.name)}>Export</a></li>
-                      <li><a href="#" onClick={(e) => this.props.handleShowModalDelete(e, blueprint)}>Delete</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="list-view-pf-main-info">
-                  <span className="pficon pficon-template list-pf-icon-small"></span>
-                  <div className="list-view-pf-body">
-                    <div className="list-view-pf-description">
-                      <div className="list-group-item-heading">
-                        <Link to={`/blueprint/${blueprint.name}`}>{blueprint.name}</Link>
-                      </div>
-                      <div className="list-group-item-text">
-                        {blueprint.description}
-                      </div>
+                      Test<strong>2</strong></div>
+                    <div
+                      className="list-view-pf-additional-info-item list-view-pf-additional-info-item-stacked hidden"
+                    >
+                      Development<strong>0</strong>
                     </div>
-                    <div className="list-view-pf-additional-info hidden">
-                      <div
-                        className="list-view-pf-additional-info-item list-view-pf-additional-info-item-stacked hidden"
-                      >
-                        Test<strong>2</strong></div>
-                      <div
-                        className="list-view-pf-additional-info-item list-view-pf-additional-info-item-stacked hidden"
-                      >
-                        Development<strong>0</strong>
-                      </div>
-                      <div
-                        className="list-view-pf-additional-info-item list-view-pf-additional-info-item-stacked hidden"
-                      >
-                        Production<strong>1</strong>
-                      </div>
+                    <div
+                      className="list-view-pf-additional-info-item list-view-pf-additional-info-item-stacked hidden"
+                    >
+                      Production<strong>1</strong>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="list-group-item-container container-fluid hidden">
-                <div className="close hidden">
-                  <span className="pficon pficon-close"></span>
-                </div>
-                <div className="row">
-                  <div className="col-sm-12">
-                    <div className="list-group list-view-pf list-view-pf-view">
-                      <div className="list-group-item">
-                        <div className="list-view-pf-checkbox">
-                          <input type="checkbox" />
+            </div>
+
+            <div className="list-group-item-container container-fluid hidden">
+              <div className="close hidden">
+                <span className="pficon pficon-close"></span>
+              </div>
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="list-group list-view-pf list-view-pf-view">
+                    <div className="list-group-item">
+
+                      <div className="list-view-pf-checkbox">
+                        <input type="checkbox" />
+                      </div>
+                      <div className="list-view-pf-actions">
+                        <button className="btn btn-default">View Blueprint</button>
+                        <button className="btn btn-default">Download</button>
+                        <div className="dropdown pull-right dropdown-kebab-pf">
+                          <button
+                            className="btn btn-link dropdown-toggle"
+                            type="button"
+                            id="dropdownKebabRight12"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="true"
+                          >
+                            <span className="fa fa-ellipsis-v"></span>
+                          </button>
+                          <ul
+                            className="dropdown-menu dropdown-menu-right"
+                            aria-labelledby="dropdownKebabRight12"
+                          >
+                            <li><a >Action</a></li>
+                            <li><a >Another action</a></li>
+                            <li><a >Something else here</a></li>
+                            <li role="separator" className="divider"></li>
+                            <li><a >Separated link</a></li>
+                          </ul>
                         </div>
-                        <div className="list-view-pf-actions">
-                          <button className="btn btn-default">View Blueprint</button>
-                          <button className="btn btn-default">Download</button>
-                          <div className="dropdown pull-right dropdown-kebab-pf">
-                            <button
-                              className="btn btn-link dropdown-toggle"
-                              type="button"
-                              id="dropdownKebabRight12"
-                              data-toggle="dropdown"
-                              aria-haspopup="true"
-                              aria-expanded="true"
-                            >
-                              <span className="fa fa-ellipsis-v"></span>
-                            </button>
-                            <ul
-                              className="dropdown-menu dropdown-menu-right"
-                              aria-labelledby="dropdownKebabRight12"
-                            >
-                              <li><a >Action</a></li>
-                              <li><a >Another action</a></li>
-                              <li><a >Something else here</a></li>
-                              <li role="separator" className="divider"></li>
-                              <li><a >Separated link</a></li>
-                            </ul>
-                          </div>
+                      </div>
+                      <div className="list-view-pf-main-info">
+                        <div className="list-view-pf-left">
+                          <span className="fa fa-linux list-view-pf-icon-sm"></span>
                         </div>
-                        <div className="list-view-pf-main-info">
-                          <div className="list-view-pf-left">
-                            <span className="fa fa-linux list-view-pf-icon-sm"></span>
-                          </div>
-                          <div className="list-view-pf-body">
-                            <div className="list-view-pf-description">
-                              <div className="list-group-item-heading">
-                                Image 1
-                              </div>
-                              <div className="list-group-item-text">
-                                Created from Version 1
-                              </div>
+                        <div className="list-view-pf-body">
+                          <div className="list-view-pf-description">
+                            <div className="list-group-item-heading">
+                              Image 1
+                            </div>
+                            <div className="list-group-item-text">
+                              Created from Version 1
                             </div>
                           </div>
                         </div>
