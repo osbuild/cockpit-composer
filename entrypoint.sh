3194394f0607af5c4b3d726f9d3d0b1672e4f85a
Xiaofeng Wang <henrywangxf@me.com>
2018-07-04 10:52:07 +0800

    Add lorax-composer test and remove stand alone welder-web test.
    1. Add lorax-composer test and name it lorax-test.
    2. Remove stand alone welder-web test, all of UI test are based on cockpit now.
    3. Change test name from cockpit-test to bdcs-test.
    4. Change docker image name from welder/web to welder/web-lorax.
    5. Change docker image name from welder/web-cockpit to welder/web-bdcs.
    6. Update waittimeout to 90 seconds.
    7. Remove two lines of code in test_with_lorax_composer section of Makefile because they're default now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-export API_URL=${API_URL:-}
+# Make sure the folder exists
+mkdir -p /run/weldr
+# Set permissions
+chmod u=rwX,g=rwX,o=--- /run/weldr
 
-# Rewrite config.json to use relative URLs for the API requests
-cd /welder/ || exit 1
-if [ -z "$API_URL" ]; then
-    sed -i "s,var welderApiRelative=.*,var welderApiRelative=true;," ./public/js/config.js
-else
-    # Split the API url into scheme host port (ALL MUST BE PRESENT)
-    SCHEME=${API_URL%%://*}
-    NO_SCHEME=${API_URL##$SCHEME://}
-    HOST=${NO_SCHEME%%:*}
-    P1=${NO_SCHEME##*:}
-    PORT=${P1%%/}
-
-    if [ -z "$SCHEME" ] || [ -z "$HOST" ] || [ -z "$PORT" ]; then
-        echo "ERROR PARSING API_URL=$API_URL";
-        exit 1;
-    fi
-
-    sed -i "s,var welderApiScheme=.*,var welderApiScheme=$SCHEME;," ./public/js/config.js
-    sed -i "s,var welderApiHost=.*,var welderApiHost=\"$HOST\";," ./public/js/config.js
-    sed -i "s,var welderApiPort=.*,var welderApiPort=$PORT;," ./public/js/config.js
-fi
-echo "Running with config.js settings:"
-cat ./public/js/config.js
+# Run lorax-composer
+lorax-composer --group root /blueprints &
 
 # Execute the CMD
 exec "$@"
