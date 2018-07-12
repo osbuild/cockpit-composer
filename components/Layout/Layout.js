74c40a6d70bf2b307a57a3941f51ef880921c59a
Jennifer Giardino <jgiardin@redhat.com>
2018-07-12 16:15:51 -0400

    Remove unused Layout components
    Removes Header and logic for displaying Header based
    on whether this is in the context of Cockpit or not. Header
    and this logic can be added back if this ever becomes a
    stand-alone app.
    Updates styles for toast notifications to be visually
    consistent with PatternFly. PF styles are based on
    the presence of a header. Updated styles are based on
    this app loading in an iframe with the header outside the
    iframe.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from './Header';
 import Notification from '../../components/Notifications/Notification';
 import NotificationsApi from '../../data/NotificationsApi';
 import utils from '../../core/utils';
@@ -20,15 +19,9 @@ class Layout extends React.Component {
     this.setState({ notifications: NotificationsApi.getNotifications() });
   }
 
-  headerClass() {
-    if (utils.inCockpit) { return 'hidden-nav'; }
-    return '';
-  }
-
   render() {
     return (
-      <div className={this.headerClass()}>
-        {! utils.inCockpit && <Header />}
+      <div>
         {this.state.notifications &&
           <div className="toast-notifications-list-pf">
             {this.state.notifications.map((notification, i) =>
