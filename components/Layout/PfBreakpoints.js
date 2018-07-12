0971e2e641421256aeb92b7dbcecde659cbb4cc1
Jennifer Giardino <jgiardin@redhat.com>
2018-07-12 16:15:51 -0400

    Remove unused Layout components
    Removes Header and logic for displaying Header based
    on whether this is in the context of Cockpit or not. Header
    and this logic can be added back if this ever becomes a
    stand-alone app.
    Updates styles for toast notifications to be visually
    consistent with PatternFly. PF styles are based on
    the presence of a header. Updated styles are based on
    this app loading in an iframe with the header outside the
    iframe.

--- components/Layout/PfBreakpoints.js
+++
@@ -1,9 +0,0 @@
-/* global jQuery */
-
-// Util: definition of breakpoint sizes for tablet and desktop modes
-(($ => {
-  $.pfBreakpoints = { // eslint-disable-line no-param-reassign
-    tablet: 768,
-    desktop: 1200,
-  };
-})(jQuery));
