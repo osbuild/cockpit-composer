27c9ae47710352084cc5f18670f658d77171a4fb
Xiaofeng Wang <henrywangxf@me.com>
2019-01-08 21:38:36 +0800

    Add source page checking case.

---
+++ test/end-to-end/pages/sources.page.js
@@ -0,0 +1,35 @@
+class SourcesPage {
+  constructor() {
+    this.containerSelector = '[id="cmpsr-modal-manage-sources"]';
+  }
+
+  loading() {
+    browser.waitUntil(
+      () => browser.isExisting(this.containerSelector),
+      timeout,
+      `Loading Blueprints page failed because selector ${this.containerSelector} cannot be found`
+    );
+  }
+
+  get title() {
+    const selector = `${this.containerSelector} [id="myModalLabel"] span`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Title in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get closeButton() {
+    const selector = `${this.containerSelector} .modal-footer button span`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Close button in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+}
+
+module.exports = new SourcesPage();
