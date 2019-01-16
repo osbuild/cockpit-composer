7bdc497203716643ad8add8bd6b8463b46a9018b
Jennifer Giardino <jgiardin@redhat.com>
2019-01-15 22:02:30 -0500

    Removes unnecessary import

--- components/Modal/CreateBlueprint.js
+++ components/Modal/CreateBlueprint.js
@@ -4,7 +4,6 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import BlueprintApi from "../../data/BlueprintApi";
 import {
   setModalCreateBlueprintErrorNameVisible,
   setModalCreateBlueprintErrorDuplicateVisible,
