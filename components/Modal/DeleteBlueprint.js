31b3f775cb8da255364859be1a61e401afeddf17
Katerina Koukiou <kkoukiou@redhat.com>
2019-04-15 22:35:22 +0200

    Refactor DeleteBlueprint modal dialog to use Modal from PF react
    The following changes where also introduced:
    * The link that triggers the dialog was moved to the same component as
    the modal
    * All delete blueprint modal related fields where removed from the redux
    store, and similarly all related saga actions
    * Stop using jquery to show/hide the modal; instead use event handlers on
    the buttons
    * Update test case

--- components/Modal/DeleteBlueprint.js
+++ components/Modal/DeleteBlueprint.js
@@ -1,83 +1,101 @@
-/* global $ */
-
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { Modal } from "patternfly-react";
+import { connect } from "react-redux";
+import { FormattedMessage, injectIntl } from "react-intl";
 import PropTypes from "prop-types";
 
+import { deletingBlueprint } from "../../core/actions/blueprints";
+
 class DeleteBlueprint extends React.Component {
-  componentDidMount() {
-    $(this.modal).modal("show");
-    $(this.modal).on("hidden.bs.modal", this.props.handleHideModal);
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  handleDelete(event, blueprint) {
+    this.props.deletingBlueprint(blueprint);
+    this.close();
   }
 
   render() {
     return (
-      <div
-        className="modal fade"
-        id="cmpsr-modal-delete"
-        ref={c => {
-          this.modal = c;
-        }}
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="myModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal">
-                <span className="pficon pficon-close" />
-              </button>
-              <h4 className="modal-title" id="myModalLabel">
-                <FormattedMessage defaultMessage="Delete Blueprint" />
-              </h4>
-            </div>
-            <div className="modal-body">
-              <p className="lead">
-                <FormattedMessage
-                  defaultMessage="Are you sure you want to delete the blueprint {name}?"
-                  values={{
-                    name: <strong>{this.props.blueprint.name}</strong>
-                  }}
-                />
-              </p>
-              <FormattedMessage defaultMessage="This action cannot be undone." tagName="p" />
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-default" data-dismiss="modal">
-                <FormattedMessage defaultMessage="Cancel" />
-              </button>
-              <button
-                type="button"
-                className="btn btn-danger"
-                data-dismiss="modal"
-                onClick={e => this.props.handleDelete(e, this.props.blueprint.id)}
-              >
-                <FormattedMessage defaultMessage="Delete" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <a href="#" onClick={this.open}>
+          <FormattedMessage defaultMessage="Delete" />
+        </a>
+        <Modal show={this.state.showModal} onHide={this.close} id="cmpsr-modal-delete">
+          <Modal.Header>
+            <Modal.CloseButton onClick={this.close} />
+            <Modal.Title>
+              <FormattedMessage defaultMessage="Delete Blueprint" />
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p className="lead">
+              <FormattedMessage
+                defaultMessage="Are you sure you want to delete the blueprint {name}?"
+                values={{
+                  name: <strong>{this.props.blueprint.name}</strong>
+                }}
+              />
+            </p>
+            <FormattedMessage defaultMessage="This action cannot be undone." tagName="p" />
+          </Modal.Body>
+          <Modal.Footer>
+            <button type="button" className="btn btn-default" onClick={this.close}>
+              <FormattedMessage defaultMessage="Cancel" />
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              data-dismiss="modal"
+              onClick={e => this.handleDelete(e, this.props.blueprint.id)}
+            >
+              <FormattedMessage defaultMessage="Delete" />
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </React.Fragment>
     );
   }
 }
 
+const makeMapStateToProps = state => {
+  return state;
+};
+
 DeleteBlueprint.propTypes = {
+  deletingBlueprint: PropTypes.func,
   blueprint: PropTypes.shape({
     id: PropTypes.string,
-    name: PropTypes.string,
-    visible: PropTypes.bool
-  }),
-  handleHideModal: PropTypes.func,
-  handleDelete: PropTypes.func
+    name: PropTypes.string
+  })
 };
 
 DeleteBlueprint.defaultProps = {
-  blueprint: {},
-  handleHideModal: function() {},
-  handleDelete: function() {}
+  deletingBlueprint: function() {},
+  blueprint: {}
 };
 
-export default DeleteBlueprint;
+const mapDispatchToProps = dispatch => ({
+  deletingBlueprint: blueprint => {
+    dispatch(deletingBlueprint(blueprint));
+  }
+});
+
+export default connect(
+  makeMapStateToProps,
+  mapDispatchToProps
+)(injectIntl(DeleteBlueprint));
