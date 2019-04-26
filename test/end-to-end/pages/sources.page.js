728a5508c2daf924120273b0fe28602f30691367
Xiaofeng Wang <henrywangxf@me.com>
2019-04-26 13:57:06 +0800

    Add test case for manage sources page
    Due to issue #615, some tests will be skipped on Edge browser

--- test/end-to-end/pages/sources.page.js
+++ test/end-to-end/pages/sources.page.js
@@ -1,6 +1,7 @@
 class SourcesPage {
   constructor() {
     this.containerSelector = '[id="cmpsr-modal-manage-sources"]';
+    this.addSourceContainerSelector = '[id="cmpsr-form-add-source"]';
   }
 
   loading() {
@@ -21,12 +22,183 @@ class SourcesPage {
     return $(selector);
   }
 
+  get xButton() {
+    const selector = `${this.containerSelector} button[class="close"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `X button in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get addSourceButton() {
+    const selector = `${this.containerSelector} [value="Add Source"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Add Source button in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get sourceNameList() {
+    const selector = ".cmpsr-list-sources .list-pf-item .list-pf-title";
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Source name in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $$(selector);
+  }
+
+  sourceName(name) {
+    const selector = `${this.containerSelector} [data-source=${name}] .list-pf-title`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Source name in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  sourceUrl(name) {
+    const selector = `${this.containerSelector} [data-source=${name}] .list-pf-additional-content`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Source URL in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  sourceType(name) {
+    const selector = `${this.containerSelector} [data-source=${name}] .list-pf-description span strong`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Source type in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  editButton(name) {
+    const selector = `${this.containerSelector} [data-source=${name}] [aria-label="Edit Source ${name}"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Edit button in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  moreButton(name) {
+    const selector = `${this.containerSelector} [data-source=${name}] [aria-label="Source Actions ${name}"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `: button in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
   get closeButton() {
     const selector = `${this.containerSelector} .modal-footer button span`;
     browser.waitUntil(
       () => browser.isExisting(selector),
       timeout,
       `Close button in Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  // Add source
+  get sourceNameInput() {
+    const selector = `${this.addSourceContainerSelector} [id="textInput1-modal-source"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Name input box in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get sourcePathInput() {
+    const selector = `${this.addSourceContainerSelector} [id="textInput2-modal-source"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Source path input box in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get duplicatedPathWarning() {
+    const selector = `${this.addSourceContainerSelector} [id="textInput2-modal-source-help"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Warning for duplicated path in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get sourceTypeSelect() {
+    const selector = `${this.addSourceContainerSelector} [id="textInput3-modal-source"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Source type select in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get checkSSLCertificateCheckbox() {
+    const selector = `${this.addSourceContainerSelector} [id="checkboxInput4-modal-source"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `SSL certificate checkbox in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get checkGPGKeyCheckbox() {
+    const selector = `${this.addSourceContainerSelector} [id="checkboxInput5-modal-source"]`;
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `GPG Key checkbox in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get addButton() {
+    const selector = "button=Add Source";
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Add Source button in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get updateButton() {
+    const selector = "button=Update Source";
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Update Source button in Add Sources dialog cannot be found by selector ${selector}`
+    );
+    return $(selector);
+  }
+
+  get cancelButton() {
+    const selector = "button=Cancel";
+    browser.waitUntil(
+      () => browser.isExisting(selector),
+      timeout,
+      `Cancel button in Add Sources dialog cannot be found by selector ${selector}`
     );
     return $(selector);
   }
--- test/end-to-end/specs/sources.test.js
+++ test/end-to-end/specs/sources.test.js
@@ -1,24 +1,175 @@
 const commands = require("../utils/commands");
+const wdioConfig = require("../wdio.conf.js");
 
 const blueprintsPage = require("../pages/blueprints.page");
 const sourcesPage = require("../pages/sources.page");
 
 describe("Sources Page", function() {
-  before(function() {
-    commands.login();
-    commands.startLoraxIfItDoesNotStart();
-    blueprintsPage.moreButton.click();
-    blueprintsPage.viewSourcesItem.click();
-    sourcesPage.loading();
-  });
+  const repo = "https://download.opensuse.org/repositories/shells:/fish:/release:/3/Fedora_29/";
+  const repoName = "fish";
+  describe("System source checking", function() {
+    before(function() {
+      commands.login();
+      commands.startLoraxIfItDoesNotStart();
+      blueprintsPage.moreButton.click();
+      blueprintsPage.viewSourcesItem.click();
+      sourcesPage.loading();
+    });
 
-  it("should show correct title", function() {
-    expect(sourcesPage.title.getText()).to.equal("Sources");
-  });
+    after(function() {
+      sourcesPage.closeButton.click();
+      browser.waitUntil(
+        () => !browser.isExisting(sourcesPage.containerSelector),
+        timeout,
+        "Cannot close Sources dialog"
+      );
+    });
 
-  it("close Sources dialog by clicking Close button", function() {
-    sourcesPage.closeButton.click();
-    browser.waitUntil(() => !$(sourcesPage.containerSelector).isExisting(), timeout, "Cannot close Sources dialog");
-    expect($(sourcesPage.containerSelector).isExisting()).to.be.false;
+    it("should show correct title", function() {
+      expect(sourcesPage.title.getText()).to.equal("Sources");
+    });
+
+    it("should have correct system source name", function() {
+      // get system source list from API
+      const endpoint = "/api/v0/projects/source/list";
+      const result = commands.apiFetchTest(endpoint).value;
+      // result looks like:
+      // https://github.com/weldr/lorax/blob/b57de934681056aa4f9bd480a34136cf340f510a/src/pylorax/api/v0.py#L513
+      const resultSources = JSON.parse(result.data).sources;
+
+      const nameList = sourcesPage.sourceNameList.map(source => source.getText());
+      expect(nameList).deep.equal(resultSources);
+    });
+    describe("* valication test", function() {
+      beforeEach(function() {
+        sourcesPage.addSourceButton.click();
+        sourcesPage.sourceNameInput.waitForExist(timeout);
+      });
+
+      afterEach(function() {
+        sourcesPage.cancelButton.click();
+      });
+
+      it("should focus on Name input box", function() {
+        expect(sourcesPage.sourceNameInput.hasFocus()).to.be.true;
+      });
+
+      it("source cannot be created without name", function() {
+        sourcesPage.sourcePathInput.setValue(repo);
+        sourcesPage.sourceTypeSelect.selectByValue("yum-baseurl");
+        expect(sourcesPage.addButton.getAttribute("disabled")).to.equal("true");
+      });
+
+      it("source cannot be created without path", function() {
+        sourcesPage.sourceNameInput.setValue(repoName);
+        sourcesPage.sourceTypeSelect.selectByValue("yum-baseurl");
+        expect(sourcesPage.addButton.getAttribute("disabled")).to.equal("true");
+      });
+
+      it("source cannot be created without type", function() {
+        sourcesPage.sourceNameInput.setValue(repoName);
+        sourcesPage.sourcePathInput.setValue(repo);
+        expect(sourcesPage.addButton.getAttribute("disabled")).to.equal("true");
+      });
+    });
+
+    describe("edit source test", function() {
+      before(function() {
+        // Due to issue #651, all tests in this suite will be skipped on Edge
+        if (wdioConfig.config.capabilities[0].browserName === "MicrosoftEdge") {
+          this.skip();
+        } else {
+          sourcesPage.addSourceButton.click();
+          sourcesPage.sourceNameInput.waitForExist(timeout);
+          sourcesPage.sourceNameInput.setValue(repoName);
+          sourcesPage.sourcePathInput.setValue(repo);
+          sourcesPage.sourceTypeSelect.selectByValue("yum-baseurl");
+          sourcesPage.addButton.click();
+          browser.waitUntil(() => browser.isExisting(`[data-source=${repoName}]`), timeout, "Cannot add source");
+        }
+      });
+
+      after(function() {
+        if (wdioConfig.config.capabilities[0].browserName === "MicrosoftEdge") {
+          this.skip();
+        } else {
+          if (sourcesPage.cancelButton.isExisting()) {
+            sourcesPage.cancelButton.click();
+          }
+          sourcesPage.moreButton(repoName).click();
+          browser.waitUntil(
+            () => sourcesPage.moreButton(repoName).getAttribute("aria-expanded") === "true",
+            timeout,
+            "cannot open dropdown menu"
+          );
+          browser.keys("ArrowDown");
+          browser.keys("Enter");
+          browser.waitUntil(
+            () => !browser.isExisting(`[data-source=${repoName}]`),
+            timeout * 2,
+            "Cannot delete source"
+          );
+        }
+      });
+
+      it(`source name should be ${repoName}`, function() {
+        expect(sourcesPage.sourceName(repoName).getText()).to.equal(repoName);
+      });
+
+      it(`source path should be ${repo}`, function() {
+        expect(sourcesPage.sourceUrl(repoName).getText()).to.equal(repo);
+      });
+
+      it("source type should be yum repository", function() {
+        expect(sourcesPage.sourceType(repoName).getText()).to.equal("yum repository");
+      });
+
+      it("check source from API", function() {
+        // get system source list from API
+        const endpoint = `/api/v0/projects/source/info/${repoName}`;
+        const result = commands.apiFetchTest(endpoint).value;
+        // result looks like:
+        // https://github.com/weldr/lorax/blob/b57de934681056aa4f9bd480a34136cf340f510a/src/pylorax/api/v0.py#L529
+        const resultSources = JSON.parse(result.data).sources[repoName];
+        expect(resultSources.name).to.equal(repoName);
+        expect(resultSources.url).to.equal(repo);
+        expect(resultSources.type).to.equal("yum-baseurl");
+        expect(resultSources.system).to.be.false;
+        expect(resultSources.check_gpg).to.be.false;
+        expect(resultSources.check_ssl).to.be.false;
+      });
+
+      it("edit source - enable GPG and SSL check", function() {
+        sourcesPage.editButton(repoName).click();
+        sourcesPage.checkGPGKeyCheckbox.click();
+        sourcesPage.checkSSLCertificateCheckbox.click();
+        sourcesPage.updateButton.click();
+        sourcesPage.sourceName(repoName).waitForExist(timeout);
+
+        // get system source list from API
+        const endpoint = `/api/v0/projects/source/info/${repoName}`;
+        const result = commands.apiFetchTest(endpoint).value;
+        // result looks like:
+        // https://github.com/weldr/lorax/blob/b57de934681056aa4f9bd480a34136cf340f510a/src/pylorax/api/v0.py#L529
+        const resultSources = JSON.parse(result.data).sources[repoName];
+        expect(resultSources.name).to.equal(repoName);
+        expect(resultSources.url).to.equal(repo);
+        expect(resultSources.type).to.equal("yum-baseurl");
+        expect(resultSources.system).to.be.false;
+        expect(resultSources.check_gpg).to.be.true;
+        expect(resultSources.check_ssl).to.be.true;
+      });
+
+      it("cannot add source with duplicated path", function() {
+        sourcesPage.addSourceButton.click();
+        sourcesPage.sourceNameInput.waitForExist(timeout);
+        sourcesPage.sourceNameInput.setValue(repoName);
+        sourcesPage.sourcePathInput.setValue(repo);
+        sourcesPage.sourceTypeSelect.selectByValue("yum-baseurl");
+
+        expect(sourcesPage.addButton.getAttribute("disabled")).to.equal("true");
+        expect(sourcesPage.duplicatedPathWarning.getText()).to.equal("This source path already exists.");
+      });
+    });
   });
 });
