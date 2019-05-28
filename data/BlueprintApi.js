7cb499e24183a9397107139d5cbafff51fcedf5f
Lars Karlitski <lars@karlitski.net>
2019-05-28 11:02:20 +0200

    BlueprintsApi: Use API wrappers
    Use API wrappers instead of calling the API directly. Having all URLs in
    one file is easier to manage.

--- data/BlueprintApi.js
+++ data/BlueprintApi.js
@@ -1,5 +1,5 @@
 import NotificationsApi from "./NotificationsApi";
-import utils from "../core/utils";
+import { createBlueprintApi, fetchBlueprintContentsApi } from "../core/apiCalls";
 
 class BlueprintApi {
   constructor() {
@@ -7,21 +7,16 @@ class BlueprintApi {
   }
 
   handleCommitBlueprint(blueprint) {
-    const p = new Promise((resolve, reject) => {
-      this.postBlueprint(this.postedBlueprintData(blueprint))
-        .then(() => {
-          NotificationsApi.closeNotification(undefined, "committing");
-          NotificationsApi.displayNotification(blueprint.name, "committed");
-          resolve();
-        })
-        .catch(e => {
-          console.log(`Error committing blueprint: ${e}`);
-          NotificationsApi.closeNotification(undefined, "committing");
-          NotificationsApi.displayNotification(blueprint.name, "commitFailed");
-          reject();
-        });
-    });
-    return p;
+    return createBlueprintApi(this.postedBlueprintData(blueprint))
+      .then(() => {
+        NotificationsApi.closeNotification(undefined, "committing");
+        NotificationsApi.displayNotification(blueprint.name, "committed");
+      })
+      .catch(e => {
+        console.log(`Error committing blueprint: ${e}`);
+        NotificationsApi.closeNotification(undefined, "committing");
+        NotificationsApi.displayNotification(blueprint.name, "commitFailed");
+      });
   }
 
   postedBlueprintData(blueprint) {
@@ -39,28 +34,18 @@ class BlueprintApi {
     return blueprintData;
   }
 
-  postBlueprint(blueprint) {
-    return utils.post("/api/v0/blueprints/new", blueprint);
-  }
-
   reloadBlueprintDetails(blueprint) {
     // retrieve blueprint details that were updated during save (i.e. version)
     // and reload details in UI
-    const p = new Promise((resolve, reject) => {
-      utils
-        .get("/api/v0/blueprints/depsolve/" + blueprint.name.replace(/\s/g, "-"))
-        .then(data => {
-          const updatedBlueprint = Object.assign({}, blueprint, {
-            version: data.blueprints[0].blueprint.version
-          });
-          resolve(updatedBlueprint);
-        })
-        .catch(e => {
-          console.log(`Error fetching blueprint details: ${e}`);
-          reject();
+    return fetchBlueprintContentsApi(blueprint.name.replace(/\s/g, "-"))
+      .then(data => {
+        return Object.assign({}, blueprint, {
+          version: data.blueprints[0].blueprint.version
         });
-    });
-    return p;
+      })
+      .catch(e => {
+        console.log(`Error fetching blueprint details: ${e}`);
+      });
   }
 }
 
