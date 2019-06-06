497ae990e1590b9a2bce7a4fbb0ce6ade4eefedd
Xiaofeng Wang <henrywangxf@me.com>
2019-06-06 13:15:14 +0800

    test: Remove maxInstances from capabilities in wdio.conf.js
    The maxInstances in different capability will inherit from up
    level if it is not configured

--- test/end-to-end/wdio.conf.js
+++ test/end-to-end/wdio.conf.js
@@ -57,11 +57,6 @@ exports.config = {
   //
   capabilities: [
     {
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
-      maxInstances: 1,
-      //
       browserName: process.env.BROWSER || "firefox"
     }
   ],
