b6fc0cfa2d3aa8d58603bc0d69648f017b217497
Jenn Giardino <jgiardin@redhat.com>
2019-12-13 16:30:13 -0500

    Migrates Images list view to the PF4 data list
    Also, updates images list view to match latest design

---
+++ components/Logs/Logs.js
@@ -0,0 +1,38 @@
+import React from "react";
+// import { FormattedMessage } from "react-intl";
+import PropTypes from "prop-types";
+import {
+  Bullseye
+} from "@patternfly/react-core";
+import Loading from "../Loading/Loading";
+
+class Logs extends React.PureComponent {
+  render() {
+    let logSection;
+    if (this.props.fetchingLog) {
+      logSection = (
+        <Bullseye>
+          <Loading />
+        </Bullseye>
+      );
+    } else logSection = <pre className="pf-u-px-lg pf-u-py-md">{this.props.logContent}</pre>;
+    
+    return (
+      <>
+        {logSection}
+      </>
+    );
+  }
+}
+
+Logs.propTypes = {
+    fetchingLog: PropTypes.bool,
+    logContent: PropTypes.string
+  };
+  
+  Logs.defaultProps = {
+    fetchingLog: false,
+    logContent: ""
+  };
+
+export default Logs;
