7453cea0a0f1d4b00e8aff0891ccffed156ceb57
Jacob Kozol <jacobdkozol@gmail.com>
2020-04-28 12:19:40 +0200

    components: improve aws upload review step
    When a user chooses to upload to aws they can review the data they
    entered in the final step of the CreateImageUpload wizard. The review
    step is updated to improve how the fields are organized and presented.
    Also, there is an additional aws popover containing help text in the
    review page.

--- components/Modal/CreateImageUpload.js
+++ components/Modal/CreateImageUpload.js
@@ -28,7 +28,6 @@ import NotificationsApi from "../../data/NotificationsApi";
 import BlueprintApi from "../../data/BlueprintApi";
 import { setBlueprint } from "../../core/actions/blueprints";
 import { fetchingQueue, clearQueue, startCompose, fetchingComposeTypes } from "../../core/actions/composes";
-import { fetchingUploadProviders } from "../../core/actions/uploads";
 
 const messages = defineMessages({
   imageSizePopover: {
@@ -134,7 +133,6 @@ class CreateImageUploadModal extends React.Component {
   }
 
   componentWillMount() {
-    this.props.fetchingUploadProviders();
     if (this.props.imageTypes.length === 0) {
       this.props.fetchingComposeTypes();
     }
@@ -210,10 +208,7 @@ class CreateImageUploadModal extends React.Component {
   missingRequiredFields() {
     if (this.state.uploadService.length == 0) return true;
     if (this.state.imageName.length == 0) return true;
-    const settingsLength =
-      Object.keys(this.props.providerSettings[this.state.uploadService].auth).length +
-      Object.keys(this.props.providerSettings[this.state.uploadService].settings).length;
-    if (Object.keys(this.state.uploadSettings).length != settingsLength) return true;
+    if (Object.values(this.state.uploadSettings).some(setting => setting === "")) return true;
     for (const setting in this.state.uploadSettings) {
       if (this.state.uploadSettings[setting].length == 0) return true;
     }
@@ -313,7 +308,7 @@ class CreateImageUploadModal extends React.Component {
 
   render() {
     const { formatMessage } = this.props.intl;
-    const { blueprint, imageTypes, providerSettings } = this.props;
+    const { blueprint, imageTypes } = this.props;
     const {
       showUploadAwsStep,
       showReviewStep,
@@ -437,7 +432,7 @@ class CreateImageUploadModal extends React.Component {
             </FormGroup>
             {imageType === "ami" && awsProviderCheckbox}
             <div className="pf-c-form__group">
-              <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start">
+              <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start pf-m-nowrap">
                 <label htmlFor="create-image-size" className="pf-l-flex__item">
                   <span className="pf-c-form__label-text">
                     <FormattedMessage defaultMessage="Image size" />
@@ -511,7 +506,7 @@ class CreateImageUploadModal extends React.Component {
           <Form isHorizontal className="cc-m-wide-label">
             <div className="pf-c-form__group">
               <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="access-key-id-input" className="pf-c-form__label-text">
+                <label htmlFor="access-key-id-input" className="pf-l-flex__item">
                   <span className="pf-c-form__label-text">
                     <FormattedMessage defaultMessage="Access key ID" />
                   </span>
@@ -547,7 +542,7 @@ class CreateImageUploadModal extends React.Component {
             </div>
             <div className="pf-c-form__group">
               <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="secret-access-key-input" className="pf-c-form__label-text">
+                <label htmlFor="secret-access-key-input" className="pf-l-flex__item">
                   <span className="pf-c-form__label-text">
                     <FormattedMessage defaultMessage="Secret access key" />
                   </span>
@@ -596,7 +591,7 @@ class CreateImageUploadModal extends React.Component {
           <Form isHorizontal className="cc-m-wide-label">
             <div className="pf-c-form__group">
               <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="image-name-input" className="pf-c-form__label-text">
+                <label htmlFor="image-name-input" className="pf-l-flex__item">
                   <span className="pf-c-form__label-text">
                     <FormattedMessage defaultMessage="Image name" />
                   </span>
@@ -628,7 +623,7 @@ class CreateImageUploadModal extends React.Component {
             </div>
             <div className="pf-c-form__group">
               <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="bucket-input" className="pf-c-form__label-text">
+                <label htmlFor="bucket-input" className="pf-l-flex__item">
                   <span className="pf-c-form__label-text">Amazon S3 bucket</span>
                   <span className="pf-c-form__label-required" aria-hidden="true">
                     &#42;
@@ -671,7 +666,7 @@ class CreateImageUploadModal extends React.Component {
             </div>
             <div className="pf-c-form__group">
               <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="region-input" className="pf-c-form__label-text">
+                <label htmlFor="region-input" className="pf-l-flex__item">
                   <span className="pf-c-form__label-text">
                     <FormattedMessage defaultMessage="AWS region" />
                   </span>
@@ -715,6 +710,84 @@ class CreateImageUploadModal extends React.Component {
       steps: [awsUploadAuth, awsUploadSettings]
     };
 
+    const awsReviewStep = uploadService === "aws" && (
+      <TextContent>
+        <div className="pf-l-flex">
+          <h3 className="pf-l-flex__item pf-u-mt-2xl pf-u-mb-md">
+            <FormattedMessage defaultMessage="Upload to Amazon" />
+          </h3>
+          <Popover
+            className="pf-l-flex__item"
+            id="popover-help"
+            bodyContent={
+              <TextContent>
+                <p>
+                  <FormattedMessage
+                    defaultMessage="
+                      Image Builder can upload images you create to an {bucket} in AWS and then import them into EC2. When the image build is complete
+                      and the upload action is successful, the image file is available in the AMI section of EC2. Most of the values required to upload
+                      the image can be found in the {console}.
+                    "
+                    values={{
+                      bucket: "S3 bucket",
+                      console: <a href="https://console.aws.amazon.com/console/home">AWS Management Console</a>
+                    }}
+                  />
+                </p>
+                <p>
+                  <FormattedMessage
+                    defaultMessage="
+                      This upload process requires that you have an {iam} role named {vmimport} to ensure that the image can
+                      be imported from the S3 {bucket} into EC2. For more details, refer to the {role}.
+                    "
+                    values={{
+                      bucket: "bucket",
+                      vmimport: <code>vmimport</code>,
+                      iam: "Identity and Access Management (IAM)",
+                      role: (
+                        <a href="https://docs.aws.amazon.com/vm-import/latest/userguide/vmie_prereqs.html#vmimport-role">
+                          AWS Required Service Role
+                        </a>
+                      )
+                    }}
+                  />
+                </p>
+              </TextContent>
+            }
+            aria-label="aws help"
+          >
+            <Button variant="plain" aria-label="aws help">
+              <OutlinedQuestionCircleIcon id="popover-icon" />
+            </Button>
+          </Popover>
+        </div>
+        <TextList className="cc-m-column__fixed-width" component={TextListVariants.dl}>
+          <TextListItem component={TextListItemVariants.dt}>
+            <FormattedMessage defaultMessage="Access key ID" />
+          </TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>
+            {"*".repeat(this.state.uploadSettings["accessKeyID"].length)}
+          </TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>
+            <FormattedMessage defaultMessage="Secret access key" />
+          </TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>
+            {"*".repeat(this.state.uploadSettings["secretAccessKey"].length)}
+          </TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>
+            <FormattedMessage defaultMessage="Image name" />
+          </TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>{imageName}</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Amazon S3 bucket</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>{this.state.uploadSettings["bucket"]}</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>
+            <FormattedMessage defaultMessage="AWS region" />
+          </TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>{this.state.uploadSettings["region"]}</TextListItem>
+        </TextList>
+      </TextContent>
+    );
+
     const reviewStep = {
       name: "Review",
       component: (
@@ -732,73 +805,43 @@ class CreateImageUploadModal extends React.Component {
               </Popover>
             </Title>
             <Text>{formatMessage(messages.review)}</Text>
-            {providerSettings[uploadService] !== undefined && (
-              <TextList component={TextListVariants.dl}>
-                <dt>
-                  <FormattedMessage defaultMessage="Image size" />
-                </dt>
-                <dd>
-                  {imageSize === "" && (
-                    <div>
-                      <ExclamationCircleIcon className="cc-c-text__danger-icon" />{" "}
-                      {formatMessage(messages.warningSizeEmpty)}
-                    </div>
-                  )}
-                  {imageSize !== "" && <div>{imageSize} GB</div>}
-                  {imageSize < minImageSize && imageSize !== "" && (
-                    <div>
-                      <ExclamationCircleIcon className="cc-c-text__danger-icon" />{" "}
-                      {formatMessage(messages.warningSizeSmall, {
-                        size: minImageSize
-                      })}
-                    </div>
-                  )}
-                  {imageSize > maxImageSize && (
-                    <div>
-                      <ExclamationTriangleIcon className="cc-c-text__warning-icon" />{" "}
-                      {formatMessage(messages.warningSizeLarge)}
-                    </div>
-                  )}
-                </dd>
-                {Object.keys(providerSettings[uploadService].auth).map(key => (
-                  <React.Fragment key={key}>
-                    <TextListItem component={TextListItemVariants.dt}>
-                      {formatMessage({
-                        id: "review-settings",
-                        defaultMessage: providerSettings[uploadService].auth[key].displayText
-                      })}
-                    </TextListItem>
-                    <TextListItem component={TextListItemVariants.dd}>
-                      {providerSettings[uploadService].auth[key].isPassword &&
-                      this.state.uploadSettings[key] != undefined
-                        ? "*".repeat(this.state.uploadSettings[key].length)
-                        : this.state.uploadSettings[key]}
-                    </TextListItem>
-                  </React.Fragment>
-                ))}
-                <TextListItem component={TextListItemVariants.dt}>
-                  <FormattedMessage defaultMessage="Image name" />
-                </TextListItem>
-                <TextListItem component={TextListItemVariants.dd}>{imageName}</TextListItem>
-                {Object.keys(providerSettings[uploadService].settings).map(key => (
-                  <React.Fragment key={key}>
-                    <TextListItem component={TextListItemVariants.dt}>
-                      {formatMessage({
-                        id: "review-settings",
-                        defaultMessage: providerSettings[uploadService].settings[key].displayText
-                      })}
-                    </TextListItem>
-                    <TextListItem component={TextListItemVariants.dd}>
-                      {providerSettings[uploadService].settings[key].isPassword &&
-                      this.state.uploadSettings[key] != undefined
-                        ? "*".repeat(this.state.uploadSettings[key].length)
-                        : this.state.uploadSettings[key]}
-                    </TextListItem>
-                  </React.Fragment>
-                ))}
-              </TextList>
-            )}
+            <h3 className="pf-u-mt-2xl pf-u-mb-md">
+              <FormattedMessage defaultMessage="Image output" />
+            </h3>
+            <TextList className="cc-m-column__fixed-width" component={TextListVariants.dl}>
+              <dt>
+                <FormattedMessage defaultMessage="Image size" />
+              </dt>
+              <dd>
+                {imageSize === "" && (
+                  <div>
+                    <ExclamationCircleIcon className="cc-c-text__danger-icon" />{" "}
+                    {formatMessage(messages.warningSizeEmpty)}
+                  </div>
+                )}
+                {imageSize !== "" && <div>{imageSize} GB</div>}
+                {imageSize < minImageSize && imageSize !== "" && (
+                  <div>
+                    <ExclamationCircleIcon className="cc-c-text__danger-icon" />{" "}
+                    {formatMessage(messages.warningSizeSmall, {
+                      size: minImageSize
+                    })}
+                  </div>
+                )}
+                {imageSize > maxImageSize && (
+                  <div>
+                    <ExclamationTriangleIcon className="cc-c-text__warning-icon" />{" "}
+                    {formatMessage(messages.warningSizeLarge)}
+                  </div>
+                )}
+              </dd>
+              <TextListItem component={TextListItemVariants.dt}>
+                <FormattedMessage defaultMessage="Image type" />
+              </TextListItem>
+              <TextListItem component={TextListItemVariants.dd}>{imageType}</TextListItem>
+            </TextList>
           </TextContent>
+          {awsReviewStep}
         </React.Fragment>
       )
     };
@@ -911,8 +954,6 @@ CreateImageUploadModal.propTypes = {
   clearQueue: PropTypes.func,
   imageTypes: PropTypes.arrayOf(PropTypes.object),
   fetchingComposeTypes: PropTypes.func,
-  providerSettings: PropTypes.objectOf(PropTypes.object),
-  fetchingUploadProviders: PropTypes.func,
   setBlueprint: PropTypes.func,
   intl: intlShape.isRequired,
   startCompose: PropTypes.func,
@@ -929,9 +970,7 @@ CreateImageUploadModal.defaultProps = {
   fetchingQueue: function() {},
   clearQueue: function() {},
   imageTypes: [],
-  providerSettings: {},
   fetchingComposeTypes: function() {},
-  fetchingUploadProviders: function() {},
   setBlueprint: function() {},
   startCompose: function() {},
   layout: {},
@@ -941,8 +980,7 @@ CreateImageUploadModal.defaultProps = {
 const mapStateToProps = state => ({
   composeQueue: state.composes.queue,
   composeQueueFetched: state.composes.queueFetched,
-  imageTypes: state.composes.composeTypes,
-  providerSettings: state.uploads.providerSettings
+  imageTypes: state.composes.composeTypes
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -951,9 +989,6 @@ const mapDispatchToProps = dispatch => ({
   },
   fetchingComposeTypes: () => {
     dispatch(fetchingComposeTypes());
-  },
-  fetchingUploadProviders: () => {
-    dispatch(fetchingUploadProviders());
   },
   fetchingQueue: () => {
     dispatch(fetchingQueue());
