df2c3b9c8c7097c2ff4164e745719747d645b565
Jacob Kozol <jacobdkozol@gmail.com>
2020-05-27 16:33:07 +0200

    toolbar: add translations to filter input

--- components/Toolbar/FilterInput.js
+++ components/Toolbar/FilterInput.js
@@ -1,6 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Filter } from "patternfly-react";
+import { defineMessages, injectIntl, intlShape } from "react-intl";
+
+const messages = defineMessages({
+  filterNameTitle: {
+    defaultMessage: "Name"
+  },
+  filterNamePlaceholder: {
+    defaultMessage: "Filter by Name"
+  },
+  filterReleaseTitle: {
+    defaultMessage: "Release"
+  },
+  filterReleasePlaceholder: {
+    defaultMessage: "Filter by Release"
+  },
+  filterVersionTitle: {
+    defaultMessage: "Version"
+  },
+  filterVersionPlaceholder: {
+    defaultMessage: "Filter by Version"
+  }
+});
 
 class FilterInput extends React.Component {
   constructor() {
@@ -41,31 +63,80 @@ class FilterInput extends React.Component {
   render() {
     const { emptyState, filters } = this.props;
     const currentFilterType = filters.filterTypes.find(type => type.id === this.state.currentFilterId);
+    const { formatMessage } = this.props.intl;
+
+    const filterGroup = filterID => {
+      switch (filterID) {
+        case "name":
+          return (
+            <div className="input-group">
+              <Filter.TypeSelector
+                filterTypes={filters.filterTypes}
+                currentFilterType={formatMessage(messages.filterNameTitle)}
+                onFilterTypeSelected={this.selectFilterType}
+              />
+              <input
+                onChange={this.handleChangeFilter}
+                disabled={emptyState}
+                type="text"
+                className="form-control"
+                id="filter-blueprints"
+                value={this.state.filterValue}
+                placeholder={`${formatMessage(messages.filterNamePlaceholder)}...`}
+                aria-label={formatMessage(messages.filterNamePlaceholder)}
+              />
+            </div>
+          );
+        case "release":
+          return (
+            <div className="input-group">
+              <Filter.TypeSelector
+                filterTypes={filters.filterTypes}
+                currentFilterType={formatMessage(messages.filterReleaseTitle)}
+                onFilterTypeSelected={this.selectFilterType}
+              />
+              <input
+                onChange={this.handleChangeFilter}
+                disabled={emptyState}
+                type="text"
+                className="form-control"
+                id="filter-blueprints"
+                value={this.state.filterValue}
+                placeholder={`${formatMessage(messages.filterReleasePlaceholder)}...`}
+                aria-label={formatMessage(messages.filterReleasePlaceholder)}
+              />
+            </div>
+          );
+        case "version":
+          return (
+            <div className="input-group">
+              <Filter.TypeSelector
+                filterTypes={filters.filterTypes}
+                currentFilterType={formatMessage(messages.filterVersionTitle)}
+                onFilterTypeSelected={this.selectFilterType}
+              />
+              <input
+                onChange={this.handleChangeFilter}
+                disabled={emptyState}
+                type="text"
+                className="form-control"
+                id="filter-blueprints"
+                value={this.state.filterValue}
+                placeholder={`${formatMessage(messages.filterVersionPlaceholder)}...`}
+                aria-label={formatMessage(messages.filterVersionPlaceholder)}
+              />
+            </div>
+          );
+        default:
+          return null;
+      }
+    };
+
     return (
       <form onSubmit={e => this.handleSubmitFilter(e)}>
         <div className="filter-pf-fields">
           <div className="form-group toolbar-pf-filter">
-            <div className="input-group">
-              {currentFilterType !== undefined && filters.filterTypes.length > 1 && (
-                <Filter.TypeSelector
-                  filterTypes={filters.filterTypes}
-                  currentFilterType={currentFilterType}
-                  onFilterTypeSelected={this.selectFilterType}
-                />
-              )}
-              {currentFilterType !== undefined && (
-                <input
-                  onChange={this.handleChangeFilter}
-                  disabled={emptyState}
-                  type="text"
-                  className="form-control"
-                  id="filter-blueprints"
-                  value={this.state.filterValue}
-                  placeholder={`${currentFilterType.placeholder}...`}
-                  aria-label={currentFilterType.placeholder}
-                />
-              )}
-            </div>
+            {currentFilterType !== undefined && filterGroup(currentFilterType.id)}
           </div>
         </div>
       </form>
@@ -80,7 +151,8 @@ FilterInput.propTypes = {
     filterTypes: PropTypes.arrayOf(PropTypes.object),
     filterValues: PropTypes.arrayOf(PropTypes.object)
   }),
-  filterAddValue: PropTypes.func
+  filterAddValue: PropTypes.func,
+  intl: intlShape.isRequired
 };
 
 FilterInput.defaultProps = {
@@ -89,4 +161,4 @@ FilterInput.defaultProps = {
   filterAddValue: function() {}
 };
 
-export default FilterInput;
+export default injectIntl(FilterInput);
