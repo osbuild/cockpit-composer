146a08bf273234ea8caf082b0d14ad3c25cd99ea
Jacob Kozol <jacobdkozol@gmail.com>
2020-05-27 17:07:16 +0200

    toolbar: add translations to active filters

--- components/Toolbar/ToolbarLayout.js
+++ components/Toolbar/ToolbarLayout.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { defineMessages, FormattedMessage, intlShape, injectIntl } from "react-intl";
 import PropTypes from "prop-types";
 import { Filter, Toolbar } from "patternfly-react";
+
+const messages = defineMessages({
+  filterNameID: {
+    defaultMessage: "name"
+  },
+  filterReleaseID: {
+    defaultMessage: "release"
+  },
+  filterVersionID: {
+    defaultMessage: "version"
+  }
+});
 
 class ToolbarLayout extends React.Component {
   constructor() {
@@ -15,21 +27,44 @@ class ToolbarLayout extends React.Component {
 
   render() {
     const { filters, children, filterClearValues } = this.props;
+    const { formatMessage } = this.props.intl;
+
+    const filterItem = filter => {
+      switch (filter.key) {
+        case "name":
+          return (
+            <Filter.Item key={`${filter.key}-${filter.value}`} onRemove={this.handleRemoveFilter} filterData={filter}>
+              {formatMessage(messages.filterNameID)}: {filter.value}
+            </Filter.Item>
+          );
+        case "release":
+          return (
+            <Filter.Item key={`${filter.key}-${filter.value}`} onRemove={this.handleRemoveFilter} filterData={filter}>
+              {formatMessage(messages.filterReleaseID)}: {filter.value}
+            </Filter.Item>
+          );
+        case "version":
+          return (
+            <Filter.Item key={`${filter.key}-${filter.value}`} onRemove={this.handleRemoveFilter} filterData={filter}>
+              {formatMessage(messages.filterVersionID)}: {filter.value}
+            </Filter.Item>
+          );
+        default:
+          return null;
+      }
+    };
+
     return (
       <div className="row toolbar-pf">
         <div className="col-sm-12">
           <div className="toolbar-pf-actions">{children}</div>
           {filters.filterValues && filters.filterValues.length !== 0 && (
             <Toolbar.Results>
-              <Filter.ActiveLabel>Active Filters:</Filter.ActiveLabel>
+              <Filter.ActiveLabel>
+                <FormattedMessage defaultMessage="Active Filters" />:
+              </Filter.ActiveLabel>
               <Filter.List>
-                {filters.filterValues.map(item => {
-                  return (
-                    <Filter.Item key={`${item.key}-${item.value}`} onRemove={this.handleRemoveFilter} filterData={item}>
-                      {item.key}: {item.value}
-                    </Filter.Item>
-                  );
-                })}
+                {filters.filterValues.map(filter => filterItem(filter))}
               </Filter.List>
               <button type="button" className="btn-link" onClick={filterClearValues}>
                 <FormattedMessage defaultMessage="Clear All Filters" />
@@ -50,7 +85,8 @@ ToolbarLayout.propTypes = {
   }),
   filterRemoveValue: PropTypes.func,
   filterClearValues: PropTypes.func,
-  children: PropTypes.node
+  children: PropTypes.node,
+  intl: intlShape.isRequired
 };
 
 ToolbarLayout.defaultProps = {
@@ -60,4 +96,4 @@ ToolbarLayout.defaultProps = {
   children: React.createElement("div")
 };
 
-export default ToolbarLayout;
+export default injectIntl(ToolbarLayout);
