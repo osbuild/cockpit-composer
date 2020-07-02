3d0b949cd8c0c50f1f49b88b02ae523b34edf085
Jacob Kozol <jacobdkozol@gmail.com>
2020-07-02 20:55:47 +0200

    wizard: migrate aws auth wizard step to seperate file
    The create image upload wizard should be split into multiple files. This
    improves readability and makes it simpler to refactor and expand the
    wizard and its steps. The aws auth step is now in its own seperate
    component.

--- components/Wizard/CreateImageUpload.js
+++ components/Wizard/CreateImageUpload.js
@@ -33,6 +33,7 @@ import NotificationsApi from "../../data/NotificationsApi";
 import BlueprintApi from "../../data/BlueprintApi";
 import { setBlueprint } from "../../core/actions/blueprints";
 import { fetchingQueue, clearQueue, startCompose, fetchingComposeTypes } from "../../core/actions/composes";
+import AWSAuthStep from "./AWSAuthStep";
 
 const messages = defineMessages({
   accessKeys: {
@@ -107,14 +108,6 @@ const ariaLabels = defineMessages({
   imageSize: {
     id: "image-size-help",
     defaultMessage: "Image size help",
-  },
-  accessKeyID: {
-    id: "access-key-id-help",
-    defaultMessage: "Access key ID help",
-  },
-  secretAccessKey: {
-    id: "secret-access-key-help",
-    defaultMessage: "Secret access key help",
   },
   imageName: {
     id: "image-name-help",
@@ -783,87 +776,7 @@ class CreateImageUploadModal extends React.Component {
 
     const awsUploadAuth = {
       name: "Authentication",
-      component: (
-        <>
-          <Text className="help-block cc-c-form__required-text">
-            <FormattedMessage defaultMessage="All fields are required." />
-          </Text>
-          <Form isHorizontal className="cc-m-wide-label">
-            <div className="pf-c-form__group">
-              <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-u-display-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="access-key-id-input" className="pf-l-flex__item">
-                  <span className="pf-c-form__label-text">
-                    <FormattedMessage defaultMessage="Access key ID" />
-                  </span>
-                  <span className="pf-c-form__label-required" aria-hidden="true">
-                    &#42;
-                  </span>
-                </label>
-                <Popover
-                  id="access-key-popover"
-                  bodyContent={
-                    <FormattedMessage
-                      defaultMessage="You can create and find existing Access key IDs on the {iam} page in the AWS console."
-                      values={{
-                        iam: <strong>Identity and Access Management (IAM)</strong>,
-                      }}
-                    />
-                  }
-                  aria-label={formatMessage(ariaLabels.accessKeyID)}
-                >
-                  <Button variant="plain" aria-label={formatMessage(ariaLabels.accessKeyID)}>
-                    <OutlinedQuestionCircleIcon id="popover-icon" />
-                  </Button>
-                </Popover>
-              </div>
-              <TextInput
-                className="pf-c-form-control"
-                value={this.state.uploadSettings.accessKeyID}
-                type="password"
-                id="access-key-id-input"
-                name="accessKeyID"
-                onChange={this.setUploadSettings}
-              />
-            </div>
-            <div className="pf-c-form__group">
-              <div className="pf-c-form__label pf-m-no-padding-top pf-l-flex pf-u-display-flex pf-m-justify-content-flex-start pf-m-nowrap">
-                <label htmlFor="secret-access-key-input" className="pf-l-flex__item">
-                  <span className="pf-c-form__label-text">
-                    <FormattedMessage defaultMessage="Secret access key" />
-                  </span>
-                  <span className="pf-c-form__label-required" aria-hidden="true">
-                    &#42;
-                  </span>
-                </label>
-                <Popover
-                  id="secret-key-popover"
-                  bodyContent={
-                    <FormattedMessage
-                      defaultMessage="You can view the Secret access key only when you create a new Access key ID on the {iam} page in the AWS console."
-                      values={{
-                        iam: <strong>Identity and Access Management (IAM)</strong>,
-                      }}
-                    />
-                  }
-                  aria-label={formatMessage(ariaLabels.secretAccessKey)}
-                >
-                  <Button variant="plain" aria-label={formatMessage(ariaLabels.secretAccessKey)}>
-                    <OutlinedQuestionCircleIcon id="popover-icon" />
-                  </Button>
-                </Popover>
-              </div>
-              <TextInput
-                className="pf-c-form-control"
-                value={this.state.uploadSettings.secretAccessKey}
-                type="password"
-                id="secret-access-key-input"
-                name="secretAccessKey"
-                onChange={this.setUploadSettings}
-              />
-            </div>
-          </Form>
-        </>
-      ),
+      component: <AWSAuthStep uploadSettings={this.state.uploadSettings} setUploadSettings={this.setUploadSettings} />,
     };
 
     const awsUploadSettings = {
