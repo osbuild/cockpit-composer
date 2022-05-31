bcfcc2933e0a4daf9e8f5e6876484eeeb3e61524
Jacob Kozol <jkozol@redhat.com>
2022-05-31 16:16:19 +0200

    Wizard: add basic ddf wizard
    This is a very minimal migration of the existing wizard to ddf. No
    customizations or review. Just basic image selection and creation.

---
+++ components/Wizard/formComponents/BlueprintName.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FormGroup, Text, Popover, Button } from "@patternfly/react-core";
+import { HelpIcon } from "@patternfly/react-icons";
+
+const BlueprintName = (props) => {
+  return (
+    <FormGroup
+      label="Blueprint"
+      labelIcon={
+        <Popover
+          bodyContent="This process can take a while. Images are built in the order they are started."
+          aria-label="Process length help"
+        >
+          <Button variant="plain" aria-label="Process length help">
+            <HelpIcon />
+          </Button>
+        </Popover>
+      }
+    >
+      <Text>{props.blueprint.name}</Text>
+    </FormGroup>
+  );
+};
+
+export default BlueprintName;
