ef2aca31ab8c5aae0fd49ba0ab7e23062cd7a5aa
Jacob Kozol <jkozol@redhat.com>
2022-06-02 16:38:45 +0200

    wizard: add review page

---
+++ components/Wizard/formComponents/Review.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Text,
+  TextContent,
+  TextList,
+  TextListVariants,
+  TextListItem,
+  TextListItemVariants,
+} from "@patternfly/react-core";
+import useFormApi from "@data-driven-forms/react-form-renderer/use-form-api";
+
+const Review = (props) => {
+  const { getState } = useFormApi();
+
+  return (
+    <>
+      <Text>
+        Review the information and click &quot;Create image&quot; to create the image using the following criteria.
+      </Text>
+      <TextContent>
+        <TextList component={TextListVariants.dl}>
+          <TextListItem component={TextListItemVariants.dt}>Blueprint name</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>{props.blueprint.name}</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Output type</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>{getState()?.values?.["image-output-type"]}</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Image size</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>{getState()?.values?.["image-size"]}</TextListItem>
+        </TextList>
+      </TextContent>
+    </>
+  );
+};
+
+export default Review;
