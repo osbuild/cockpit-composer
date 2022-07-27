58cdda252ee24242709ea8fae36aba89d21afc81
Jacob Kozol <jkozol@redhat.com>
2022-07-27 17:44:21 +0200

    Wizrd: improve details input
    The details page now validates the image size being entered.

--- components/Wizard/steps/details.js
+++ components/Wizard/steps/details.js
@@ -1,4 +1,7 @@
-import { componentTypes } from "@data-driven-forms/react-form-renderer";
+import React from "react";
+import validatorTypes from "@data-driven-forms/react-form-renderer/validator-types";
+import { Popover, Button } from "@patternfly/react-core";
+import { HelpIcon } from "@patternfly/react-icons";
 
 export default {
   title: "Details",
@@ -6,10 +9,87 @@ export default {
   nextStep: "review",
   fields: [
     {
-      component: componentTypes.TEXT_FIELD,
+      component: "text-field-custom",
       name: "image-size",
-      label: "Image size",
+      className: "pf-u-w-50",
       type: "number",
+      label: "Image size",
+      labelIcon: (
+        <Popover
+          bodyContent={
+            <>
+              Set the size that you want the image to be when instantiated. The total package size and target
+              destination of your image should be considered when setting the image size.
+            </>
+          }
+          aria-label="Image size help"
+        >
+          <Button variant="plain" aria-label="Image size help">
+            <HelpIcon />
+          </Button>
+        </Popover>
+      ),
+      helperText: "Minimum image size is 6GB",
+      initializeOnMount: true,
+      initialValue: 6,
+      isRequired: true,
+      autoFocus: true,
+      condition: {
+        when: "image-output-type",
+        is: "ami",
+      },
+      validate: [
+        {
+          type: validatorTypes.REQUIRED,
+        },
+        {
+          type: validatorTypes.MIN_NUMBER_VALUE,
+          includeThreshold: true,
+          value: 6,
+          message: "Minimum image size is 6GB",
+        },
+      ],
+    },
+    {
+      component: "text-field-custom",
+      name: "image-size",
+      className: "pf-u-w-50",
+      type: "number",
+      label: "Image size",
+      labelIcon: (
+        <Popover
+          bodyContent={
+            <>
+              Set the size that you want the image to be when instantiated. The total package size and target
+              destination of your image should be considered when setting the image size.
+            </>
+          }
+          aria-label="Image size help"
+        >
+          <Button variant="plain" aria-label="Image size help">
+            <HelpIcon />
+          </Button>
+        </Popover>
+      ),
+      helperText: "Minimum image size is 2GB",
+      initializeOnMount: true,
+      initialValue: 2,
+      isRequired: true,
+      autoFocus: true,
+      condition: {
+        not: [{ when: "image-output-type", is: "ami" }],
+      },
+      validate: [
+        {
+          type: validatorTypes.REQUIRED,
+        },
+        {
+          type: validatorTypes.MIN_NUMBER_VALUE,
+          includeThreshold: true,
+          value: 2,
+          message: "Minimum image size is 2GB",
+        },
+      ],
     },
   ],
 };
