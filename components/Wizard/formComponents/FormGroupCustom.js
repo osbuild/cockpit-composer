081c16083b5d24b9df5b24d17bbd0d9df8ed6814
Jacob Kozol <jkozol@redhat.com>
2022-08-02 21:43:12 +0200

    Wizard: update FormGroup props
    The data driven forms' default FormGroup does not include all props
    included by Patternfly 4. Therefore, we need to add custom overrides for
    the FormGroup and TextInput.

---
+++ components/Wizard/formComponents/FormGroupCustom.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { FormGroup as Pf4FormGroup, TextContent, Text } from "@patternfly/react-core";
+import PropTypes from "prop-types";
+
+const showError = ({ error, touched, warning, submitError }, validateOnMount) => {
+  if ((touched || validateOnMount) && error) {
+    return { validated: "error" };
+  }
+
+  if ((touched || validateOnMount) && submitError) {
+    return { validated: "error" };
+  }
+
+  if ((touched || validateOnMount) && warning) {
+    return { validated: "warning" };
+  }
+
+  return { validated: "default" };
+};
+
+const FormGroupCustom = ({
+  className,
+  label,
+  labelIcon,
+  isRequired,
+  helperText,
+  meta,
+  validateOnMount,
+  description,
+  hideLabel,
+  children,
+  id,
+  FormGroupProps,
+}) => (
+  <Pf4FormGroup
+    className={className}
+    isRequired={isRequired}
+    label={!hideLabel && label}
+    labelIcon={!hideLabel && labelIcon}
+    fieldId={id}
+    helperText={((meta.touched || validateOnMount) && meta.warning) || helperText}
+    helperTextInvalid={meta.error || meta.submitError}
+    {...showError(meta, validateOnMount)}
+    {...FormGroupProps}
+  >
+    {description && (
+      <TextContent>
+        <Text component="small">{description}</Text>
+      </TextContent>
+    )}
+    {children}
+  </Pf4FormGroup>
+);
+
+FormGroupCustom.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.node,
+  labelIcon: PropTypes.node,
+  isRequired: PropTypes.bool,
+  helperText: PropTypes.node,
+  meta: PropTypes.object.isRequired,
+  description: PropTypes.node,
+  hideLabel: PropTypes.bool,
+  validateOnMount: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]).isRequired,
+  FormGroupProps: PropTypes.object,
+};
+
+export default FormGroupCustom;
