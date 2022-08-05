5a955a8370822595c8b6857a02bcd71f2614e711
lucasgarfield <lucas@redhat.com>
2022-08-05 15:04:28 +0200

    Wizard: Add update blueprint button
    This commit adds a button on the review step that allows the user to
    update a blueprint without building an image.

--- components/Wizard/steps/review.js
+++ components/Wizard/steps/review.js
@@ -1,7 +1,27 @@
+import React from "react";
+import { Button, Tooltip } from "@patternfly/react-core";
+
+const CustomReviewFooter = (props) => {
+  console.log("props: ", props);
+  return (
+    <>
+      <Tooltip content={<div>Update the blueprint and build an image.</div>}>
+        <Button variant="primary">Create image</Button>
+      </Tooltip>
+      <Tooltip content={<div>Update the blueprint without building an image.</div>}>
+        <Button variant="secondary">Update blueprint</Button>
+      </Tooltip>
+      <Button variant="secondary">Back</Button>
+      <Button variant="link">Cancel</Button>
+    </>
+  );
+};
+
 export default {
   name: "review",
   component: "review",
   title: "Review",
+  buttons: CustomReviewFooter,
   fields: [
     {
       name: "review",
