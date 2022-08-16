24e98ca7f20bcf7a86e83395c9307973ab38af3b
Jacob Kozol <jkozol@redhat.com>
2022-08-16 12:48:47 +0200

    Wizard: consolidate image size field
    The image size field now uses the resolveProps prop to determine which
    minimum image size is required depending on what the image type is.

--- components/Wizard/steps/details.js
+++ components/Wizard/steps/details.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import validatorTypes from "@data-driven-forms/react-form-renderer/validator-types";
 import { Popover, Button } from "@patternfly/react-core";
@@ -29,50 +30,6 @@ export default {
           </Button>
         </Popover>
       ),
-      helperText: "Minimum image size is 6GB",
-      initializeOnMount: true,
-      initialValue: 6,
-      isRequired: true,
-      autoFocus: true,
-      condition: [
-        {
-          when: "image-output-type",
-          is: "ami",
-        },
-      ],
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-        {
-          type: validatorTypes.MIN_NUMBER_VALUE,
-          includeThreshold: true,
-          value: 6,
-          message: "Minimum image size is 6GB",
-        },
-      ],
-    },
-    {
-      component: "text-field-custom",
-      name: "image-size",
-      className: "pf-u-w-50",
-      type: "number",
-      label: "Image size",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Set the size that you want the image to be when instantiated. The total package size and target
-              destination of your image should be considered when setting the image size.
-            </>
-          }
-          aria-label="Image size help"
-        >
-          <Button variant="plain" aria-label="Image size help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
       helperText: "Minimum image size is 2GB",
       initializeOnMount: true,
       initialValue: 2,
@@ -80,15 +37,7 @@ export default {
       autoFocus: true,
       condition: {
         when: "image-output-type",
-        is: [
-          "ami",
-          "edge-simplified-installer",
-          "fedora-iot-commit",
-          "edge-commit",
-          "edge-container",
-          "edge-installer",
-          "image-installer",
-        ],
+        is: ["fedora-iot-commit", "edge-commit", "edge-container", "edge-installer", "image-installer"],
         notMatch: true,
       },
       validate: [
@@ -102,50 +51,43 @@ export default {
           message: "Minimum image size is 2GB",
         },
       ],
-    },
-    {
-      component: "text-field-custom",
-      name: "image-size",
-      className: "pf-u-w-50",
-      type: "number",
-      label: "Image size",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Set the size that you want the image to be when instantiated. The total package size and target
-              destination of your image should be considered when setting the image size.
-            </>
-          }
-          aria-label="Image size help"
-        >
-          <Button variant="plain" aria-label="Image size help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      helperText: "Minimum image size is 10GB",
-      initializeOnMount: true,
-      initialValue: 10,
-      isRequired: true,
-      autoFocus: true,
-      condition: [
-        {
-          when: "image-output-type",
-          is: "edge-simplified-installer",
-        },
-      ],
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-        {
-          type: validatorTypes.MIN_NUMBER_VALUE,
-          includeThreshold: true,
-          value: 10,
-          message: "Minimum image size is 10GB",
-        },
-      ],
+      resolveProps: (props, { meta, input }, formOptions) => {
+        const imageType = formOptions.getState().values["image-output-type"];
+        if (imageType === "ami") {
+          return {
+            initialValue: 6,
+            helperText: "Minimum image size is 6GB",
+            validate: [
+              {
+                type: validatorTypes.REQUIRED,
+              },
+              {
+                type: validatorTypes.MIN_NUMBER_VALUE,
+                includeThreshold: true,
+                value: 6,
+                message: "Minimum image size is 6GB",
+              },
+            ],
+          };
+        }
+        if (imageType === "edge-simplified-installer") {
+          return {
+            initialValue: 10,
+            helperText: "Minimum image size is 10GB",
+            validate: [
+              {
+                type: validatorTypes.REQUIRED,
+              },
+              {
+                type: validatorTypes.MIN_NUMBER_VALUE,
+                includeThreshold: true,
+                value: 6,
+                message: "Minimum image size is 10GB",
+              },
+            ],
+          };
+        }
+      },
     },
   ],
 };
