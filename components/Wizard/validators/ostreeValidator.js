97760348395e280de4f971408a4822e6c6f219ed
Jacob Kozol <jkozol@redhat.com>
2022-08-19 15:05:15 +0200

    wizard: translate ostreeSettings

--- components/Wizard/validators/ostreeValidator.js
+++ components/Wizard/validators/ostreeValidator.js
@@ -1,10 +1,19 @@
+/* eslint-disable react/display-name */
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
 const OSTreeValidator = () => (value, formValues) => {
   if (!value) {
     return undefined;
   }
 
   if (formValues["ostree-repo-url"]?.length > 0 && formValues["ostree-parent-commit"]?.length > 0) {
-    return "Either the parent commit or repository url can be specified. Not both.";
+    return (
+      <FormattedMessage
+        id="wizard.ostree.validationError"
+        defaultMessage="Either the parent commit or repository url can be specified. Not both."
+      />
+    );
   }
 };
 
