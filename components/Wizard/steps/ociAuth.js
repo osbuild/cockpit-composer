ebae2eadb8d798f79f57c2c1174a84108a4c8de4
Jacob Kozol <jkozol@redhat.com>
2022-08-19 13:32:30 +0200

    wizard: translate oci steps

--- components/Wizard/steps/ociAuth.js
+++ components/Wizard/steps/ociAuth.js
@@ -1,102 +1,132 @@
 import React from "react";
+import { useIntl, defineMessages, FormattedMessage } from "react-intl";
 import validatorTypes from "@data-driven-forms/react-form-renderer/validator-types";
 import { Popover, Button } from "@patternfly/react-core";
 import { HelpIcon } from "@patternfly/react-icons";
 
-export default {
-  title: "Authentication",
-  name: "oci-auth",
-  substepOf: "Upload to OCI",
-  nextStep: "oci-dest",
-  fields: [
-    {
-      component: "text-field-custom",
-      name: "oci-user-ocid",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "User OCID",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              You can find your user OCID <strong>Identity and Access Management (IAM)</strong>
-              page in the OCI console.
-            </>
-          }
-          aria-label="User ocid help"
-        >
-          <Button variant="plain" aria-label="User ocid help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      autoFocus: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "upload-file",
-      name: "oci-private-key",
-      label: "Private key",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              You can view your deployed RSA keys in the API Keys section on the{" "}
-              <strong>Identity and Access Management (IAM)</strong>
-              page in the OCI console.
-            </>
-          }
-          aria-label="Private key help"
-        >
-          <Button variant="plain" aria-label="Private key help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "text-field",
-      name: "oci-private-key-filename",
-      hideField: true,
-    },
-    {
-      component: "text-field-custom",
-      name: "oci-fingerprint",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "Fingerprint",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              You can view your deployed RSA keys fingerprint in the API Keys section on the
-              <strong>Identity and Access Management (IAM)</strong> page in the OCI console.
-            </>
-          }
-          aria-label="Fingerprint help"
-        >
-          <Button variant="plain" aria-label="Fingerprint help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-  ],
+const messages = defineMessages({
+  ociTitle: {
+    id: "wizard.oci.title",
+    defaultMessage: "Upload to OCI",
+  },
+  userOCIDPopoverBody: {
+    id: "wizard.oci.userOCID.popoverBody",
+    defaultMessage:
+      "You can find your user OCID <strong>Identity and Access Management (IAM)</strong> page in the OCI console.",
+  },
+  userOCIDPopoverAria: {
+    id: "wizard.oci.userOCID.popoverAria",
+    defaultMessage: "User ocid help",
+  },
+  privateKeyPopoverBody: {
+    id: "wizard.oci.privateKey.popoverBody",
+    defaultMessage:
+      "You can view your deployed RSA keys in the API Keys section on the <strong>Identity and Access Management (IAM)</strong> page in the OCI console.",
+  },
+  privateKeyPopoverAria: {
+    id: "wizard.oci.privateKey.popoverAria",
+    defaultMessage: "Private key help",
+  },
+  fingerprintPopoverBody: {
+    id: "wizard.oci.fingerprint.popoverBody",
+    defaultMessage:
+      "You can view your deployed RSA keys fingerprint in the API Keys section on the <strong>Identity and Access Management (IAM)</strong> page in the OCI console.",
+  },
+  fingerprintPopoverAria: {
+    id: "wizard.oci.fingerprint.popoverAria",
+    defaultMessage: "Fingerprint help",
+  },
+});
+
+const ociAuth = () => {
+  const intl = useIntl();
+  return {
+    title: <FormattedMessage id="wizard.oci.authTitle" defaultMessage="Authentication" />,
+    name: "oci-auth",
+    substepOf: intl.formatMessage(messages.ociTitle),
+    nextStep: "oci-dest",
+    fields: [
+      {
+        component: "text-field-custom",
+        name: "oci-user-ocid",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.userOCID.label" defaultMessage="User OCID" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.userOCIDPopoverBody, {
+              strong: (str) => <strong>{str}</strong>,
+            })}
+            aria-label={intl.formatMessage(messages.userOCIDPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.userOCIDPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        autoFocus: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "upload-file",
+        name: "oci-private-key",
+        label: <FormattedMessage id="wizard.oci.privateKey.label" defaultMessage="Private key" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.privateKeyPopoverBody, {
+              strong: (str) => <strong>{str}</strong>,
+            })}
+            aria-label={intl.formatMessage(messages.privateKeyPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.privateKeyPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "text-field",
+        name: "oci-private-key-filename",
+        hideField: true,
+      },
+      {
+        component: "text-field-custom",
+        name: "oci-fingerprint",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.fingerprint.label" defaultMessage="Fingerprint" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.fingerprintPopoverBody, {
+              strong: (str) => <strong>{str}</strong>,
+            })}
+            aria-label={intl.formatMessage(messages.fingerprintPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.fingerprintPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+    ],
+  };
 };
+
+export default ociAuth;
--- components/Wizard/steps/ociDest.js
+++ components/Wizard/steps/ociDest.js
@@ -1,176 +1,222 @@
 import React from "react";
+import { useIntl, defineMessages, FormattedMessage } from "react-intl";
 import validatorTypes from "@data-driven-forms/react-form-renderer/validator-types";
 import { Popover, Button } from "@patternfly/react-core";
 import { HelpIcon } from "@patternfly/react-icons";
 
-export default {
-  title: "Destination",
-  name: "oci-dest",
-  substepOf: "Upload to OCI",
-  nextStep: "details",
-  fields: [
-    {
-      component: "text-field-custom",
-      name: "oci-image-name",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "Image name",
-      labelIcon: (
-        <Popover
-          bodyContent={<>Provide a file name to be used for the image file that will be uploaded.</>}
-          aria-label="Image name help"
-        >
-          <Button variant="plain" aria-label="Image name help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      autoFocus: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "text-field-custom",
-      name: "oci-bucket",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "OCI bucket",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Provide the bucket name to which the image file will be uploaded before being imported as a custom image.
-              The bucket must already exist in the Region where you want to import your image. You can find a list of
-              buckets on the OCI buckets page in the OCI storage service in the OCI console.
-            </>
-          }
-          aria-label="OCI bucket help"
-        >
-          <Button variant="plain" aria-label="OCI bucket help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "text-field-custom",
-      name: "oci-bucket-namespace",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "Bucket namespace",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Provide the namespace of the OCI bucket your uploading to. You can find the namespace in the bucket
-              details.
-            </>
-          }
-          aria-label="Bucket namespace help"
-        >
-          <Button variant="plain" aria-label="Bucket namespace help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "text-field-custom",
-      name: "oci-bucket-region",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "Bucket region",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Provide the region of the Bucket region your uploading to. You can find the region in the bucket details.
-            </>
-          }
-          aria-label="Bucket region help"
-        >
-          <Button variant="plain" aria-label="Bucket region help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "text-field-custom",
-      name: "oci-bucket-compartment",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "Bucket compartment",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Provide the compartment of the OCI bucket compartment your uploading to. You can find the compartment in
-              the bucket details.
-            </>
-          }
-          aria-label="Bucket compartment help"
-        >
-          <Button variant="plain" aria-label="Bucket compartment help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-    {
-      component: "text-field-custom",
-      name: "oci-bucket-tenancy",
-      className: "pf-u-w-50",
-      type: "text",
-      label: "Bucket tenancy",
-      labelIcon: (
-        <Popover
-          bodyContent={
-            <>
-              Provide the tenancy of the OCI bucket your uploading to. You can find the tenancy in the bucket details.
-            </>
-          }
-          aria-label="Bucket tenancy help"
-        >
-          <Button variant="plain" aria-label="Bucket tenancy help">
-            <HelpIcon />
-          </Button>
-        </Popover>
-      ),
-      isRequired: true,
-      validate: [
-        {
-          type: validatorTypes.REQUIRED,
-        },
-      ],
-    },
-  ],
+const messages = defineMessages({
+  ociTitle: {
+    id: "wizard.oci.title",
+    defaultMessage: "Upload to OCI",
+  },
+  imageNamePopoverBody: {
+    id: "wizard.oci.imageName.popoverBody",
+    defaultMessage: "Provide a file name to be used for the image file that will be uploaded.",
+  },
+  imageNamePopoverAria: {
+    id: "wizard.oci.imageName.popoverAria",
+    defaultMessage: "Image name help",
+  },
+  bucketPopoverBody: {
+    id: "wizard.oci.bucket.popoverBody",
+    defaultMessage:
+      "Provide the bucket name to which the image file will be uploaded before being imported as a custom " +
+      "image. The bucket must already exist in the Region where you want to import your image. You can find a " +
+      "list of buckets on the <strong>OCI buckets</strong> page in the OCI storage service in the OCI console.",
+  },
+  bucketPopoverAria: {
+    id: "wizard.oci.bucket.popoverAria",
+    defaultMessage: "OCI bucket help",
+  },
+  namespacePopoverBody: {
+    id: "wizard.oci.namespace.popoverBody",
+    defaultMessage:
+      "Provide the namespace of the OCI bucket your uploading to. You can find the namespace in the bucket details.",
+  },
+  namespacePopoverAria: {
+    id: "wizard.oci.namespace.popoverAria",
+    defaultMessage: "Bucket namespace help",
+  },
+  regionPopoverBody: {
+    id: "wizard.oci.region.popoverBody",
+    defaultMessage:
+      "Provide the region of the Bucket region your uploading to. You can find the region in the bucket details.",
+  },
+  regionPopoverAria: {
+    id: "wizard.oci.region.popoverAria",
+    defaultMessage: "Bucket region help",
+  },
+  compartmentPopoverBody: {
+    id: "wizard.oci.compartment.popoverBody",
+    defaultMessage:
+      "Provide the compartment of the OCI bucket compartment your uploading to. You can find the compartment in the bucket details.",
+  },
+  compartmentPopoverAria: {
+    id: "wizard.oci.compartment.popoverAria",
+    defaultMessage: "Bucket compartment help",
+  },
+  tenancyPopoverBody: {
+    id: "wizard.oci.tenancy.popoverBody",
+    defaultMessage:
+      "Provide the tenancy of the OCI bucket your uploading to. You can find the tenancy in the bucket details.",
+  },
+  tenancyPopoverAria: {
+    id: "wizard.oci.tenancy.popoverAria",
+    defaultMessage: "Bucket tenancy help",
+  },
+});
+
+const ociDest = () => {
+  const intl = useIntl();
+  return {
+    title: <FormattedMessage id="wizard.oci.destTitle" defaultMessage="Destination" />,
+    name: "oci-dest",
+    substepOf: intl.formatMessage(messages.ociTitle),
+    nextStep: "details",
+    fields: [
+      {
+        component: "text-field-custom",
+        name: "oci-image-name",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.imageName.label" defaultMessage="Image name" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.imageNamePopoverBody)}
+            aria-label={intl.formatMessage(messages.imageNamePopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.imageNamePopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        autoFocus: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "text-field-custom",
+        name: "oci-bucket",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.bucket.label" defaultMessage="OCI bucket" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.bucketPopoverBody, {
+              strong: (str) => <strong>{str}</strong>,
+            })}
+            aria-label={intl.formatMessage(messages.bucketPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.bucketPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "text-field-custom",
+        name: "oci-bucket-namespace",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.bucketNamespace.label" defaultMessage="Bucket namespace" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.namespacePopoverBody)}
+            aria-label={intl.formatMessage(messages.namespacePopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.namespacePopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "text-field-custom",
+        name: "oci-bucket-region",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.bucketRegion.label" defaultMessage="Bucket region" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.regionPopoverBody)}
+            aria-label={intl.formatMessage(messages.regionPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.regionPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "text-field-custom",
+        name: "oci-bucket-compartment",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.bucketCompartment.label" defaultMessage="Bucket compartment" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.compartmentPopoverBody)}
+            aria-label={intl.formatMessage(messages.compartmentPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.compartmentPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+      {
+        component: "text-field-custom",
+        name: "oci-bucket-tenancy",
+        className: "pf-u-w-50",
+        type: "text",
+        label: <FormattedMessage id="wizard.oci.bucketTenancy.label" defaultMessage="Bucket tenancy" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.tenancyPopoverBody)}
+            aria-label={intl.formatMessage(messages.tenancyPopoverAria)}
+          >
+            <Button variant="plain" aria-label={intl.formatMessage(messages.tenancyPopoverAria)}>
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        isRequired: true,
+        validate: [
+          {
+            type: validatorTypes.REQUIRED,
+          },
+        ],
+      },
+    ],
+  };
 };
+
+export default ociDest;
