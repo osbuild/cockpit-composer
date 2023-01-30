c069ee834c9248d52155ff5893cecac3612bc220
regexowl <ksimicko@redhat.com>
2023-01-30 14:37:23 +0100

    Table: Add more information to the blueprint table
    Fixes #1686.
    
    This adds four new columns to the table of the blueprints. The columns are:
    - `Version` with the version of the blueprint
    - `Last image created` with date of the latest image created based on the blueprint
    - `Images built` with the number of the images built based on the blueprint
    - `Packages` with the number of packages included in the blueprint

--- src/components/Table/BlueprintTable.js
+++ src/components/Table/BlueprintTable.js
@@ -2,10 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { useIntl } from "react-intl";
-import { TableComposable, Tr, Tbody, Td } from "@patternfly/react-table";
+import {
+  TableComposable,
+  Th,
+  Thead,
+  Tr,
+  Tbody,
+  Td,
+} from "@patternfly/react-table";
 import CreateImageWizard from "../Wizard/CreateImageWizard";
 import DeleteBlueprint from "../Modal/DeleteBlueprint";
 import ExportBlueprint from "../Modal/ExportBlueprint";
+import { formTimestampLabel } from "../../helpers";
+
+const getNumberOfAssociatedImages = (images, blueprint) => {
+  return images.filter((image) => image.blueprint === blueprint.name).length;
+};
+
+const getLatestCreationDate = (images, blueprint) => {
+  const imagesByBlueprint = images.filter(
+    (image) => image.blueprint === blueprint.name
+  );
+  if (imagesByBlueprint.length > 0) {
+    const latestTimestamp = Math.max(
+      ...imagesByBlueprint.map((image) => image.job_finished)
+    );
+    return formTimestampLabel(latestTimestamp);
+  } else {
+    return "-";
+  }
+};
 
 const BlueprintTable = (props) => {
   const intl = useIntl();
@@ -13,13 +39,27 @@ const BlueprintTable = (props) => {
   return (
     <TableComposable
       aria-label={intl.formatMessage({ defaultMessage: "Blueprints table" })}
+      variant="compact"
     >
+      <Thead>
+        <Tr>
+          <Th>Name</Th>
+          <Th>Version</Th>
+          <Th>Last image created</Th>
+          <Th>Images built</Th>
+          <Th>Packages</Th>
+        </Tr>
+      </Thead>
       <Tbody>
         {props.blueprints.map((blueprint) => (
           <Tr key={blueprint.name} data-testid={blueprint.name}>
             <Td>
               <Link to={`/${blueprint.name}`}>{blueprint.name}</Link>
             </Td>
+            <Td>{blueprint.version}</Td>
+            <Td>{getLatestCreationDate(props.images, blueprint)}</Td>
+            <Td>{getNumberOfAssociatedImages(props.images, blueprint)}</Td>
+            <Td>{blueprint?.packages.length}</Td>
             <Td modifier="fitContent">
               <CreateImageWizard blueprint={blueprint} />
             </Td>
@@ -38,6 +78,7 @@ const BlueprintTable = (props) => {
 
 BlueprintTable.propTypes = {
   blueprints: PropTypes.array,
+  images: PropTypes.array,
 };
 
 export default BlueprintTable;
