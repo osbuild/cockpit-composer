5c1cdc67edc28c583f0fa93eea2a144e5c33a7d8
jkozol <jkozol@redhat.com>
2024-02-22 00:39:06 +0100

    src: add other fields card modal
    When a user enters an installation_device or hostname they are able to
    view and edit the fields from the blueprint customizations tab.

---
+++ src/forms/schemas/other.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { defineMessages, FormattedMessage } from "react-intl";
+import { Popover, Button } from "@patternfly/react-core";
+import { HelpIcon } from "@patternfly/react-icons";
+
+const messages = defineMessages({
+  installDevicePopoverBody: {
+    defaultMessage: "Specify which device the image will be installed onto.",
+  },
+  installDevicePopoverAria: {
+    defaultMessage: "Installation Device help",
+  },
+});
+
+const other = (intl) => {
+  return {
+    fields: [
+      {
+        component: "text-field-custom",
+        name: "customizations.hostname",
+        className: "pf-u-w-75",
+        type: "text",
+        label: <FormattedMessage defaultMessage="Hostname" />,
+        helperText: (
+          <FormattedMessage defaultMessage="If no hostname is provided, the hostname will be determined by the OS." />
+        ),
+      },
+      {
+        component: "text-field-custom",
+        name: "customizations.installation_device",
+        className: "pf-u-w-75",
+        label: <FormattedMessage defaultMessage="Installation Device" />,
+        labelIcon: (
+          <Popover
+            bodyContent={intl.formatMessage(messages.installDevicePopoverBody)}
+            aria-label={intl.formatMessage(messages.installDevicePopoverAria)}
+          >
+            <Button
+              variant="plain"
+              aria-label={intl.formatMessage(messages.installDevicePopoverAria)}
+            >
+              <HelpIcon />
+            </Button>
+          </Popover>
+        ),
+        helperText: (
+          <FormattedMessage defaultMessage="Enter valid device node such as /dev/sda1. Only used for the simplified-installer image type." />
+        ),
+      },
+    ],
+  };
+};
+
+export default other;
--- src/forms/steps/other.js
+++ src/forms/steps/other.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { defineMessages, FormattedMessage } from "react-intl";
-import { Popover, Button } from "@patternfly/react-core";
-import { HelpIcon } from "@patternfly/react-icons";
+import otherFields from "../schemas/other";
 
 const messages = defineMessages({
   customizationsStepTitle: {
@@ -21,40 +20,7 @@ const other = (intl) => {
     name: "other",
     substepOf: intl.formatMessage(messages.customizationsStepTitle),
     nextStep: "fdo",
-    fields: [
-      {
-        component: "text-field-custom",
-        name: "customizations.hostname",
-        className: "pf-u-w-75",
-        type: "text",
-        label: <FormattedMessage defaultMessage="Hostname" />,
-        helperText: (
-          <FormattedMessage defaultMessage="If no hostname is provided, the hostname will be determined by the OS." />
-        ),
-      },
-      {
-        component: "text-field-custom",
-        name: "customizations.installation_device",
-        className: "pf-u-w-75",
-        label: <FormattedMessage defaultMessage="Installation Device" />,
-        labelIcon: (
-          <Popover
-            bodyContent={intl.formatMessage(messages.installDevicePopoverBody)}
-            aria-label={intl.formatMessage(messages.installDevicePopoverAria)}
-          >
-            <Button
-              variant="plain"
-              aria-label={intl.formatMessage(messages.installDevicePopoverAria)}
-            >
-              <HelpIcon />
-            </Button>
-          </Popover>
-        ),
-        helperText: (
-          <FormattedMessage defaultMessage="Enter valid device node such as /dev/sda1. Only used for the simplified-installer image type." />
-        ),
-      },
-    ],
+    ...otherFields(intl),
   };
 };
 
