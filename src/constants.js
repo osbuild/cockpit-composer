4e04816748d94d833cb8b8d2a7f5755c378969a8
jkozol <jkozol@redhat.com>
2024-06-03 01:34:33 +0200

    constants: migrate mountpoints to the constants

--- src/constants.js
+++ src/constants.js
@@ -28,3 +28,47 @@ export const ImageTypeLabels = {
 export const UNIT_KIB = 1024 ** 1;
 export const UNIT_MIB = 1024 ** 2;
 export const UNIT_GIB = 1024 ** 3;
+
+export const MountPointPrefixes = [
+  "/app",
+  "/boot",
+  "/data",
+  "/home",
+  "/opt",
+  "/srv",
+  "/tmp",
+  "/usr",
+  "/usr/local",
+  "/var",
+  "/",
+];
+
+// copied from https://github.com/osbuild/images/blob/main/pkg/policies/policies.go
+export const MountPointPrefixesPolicy = {
+  "/": {},
+  // /etc must be on the root filesystem
+  "/etc": { Deny: true },
+  // NB: any mountpoints under /usr are not supported by systemd fstab
+  // generator in initram before the switch-root, so we don't allow them.
+  "/usr": { Exact: true },
+  // API filesystems
+  "/sys": { Deny: true },
+  "/proc": { Deny: true },
+  "/dev": { Deny: true },
+  "/run": { Deny: true },
+  // not allowed due to merged-usr
+  "/bin": { Deny: true },
+  "/sbin": { Deny: true },
+  "/lib": { Deny: true },
+  "/lib64": { Deny: true },
+  // used by ext filesystems
+  "/lost+found": { Deny: true },
+  // used by EFI
+  "/boot/efi": { Deny: true },
+  // used by systemd / ostree
+  "/sysroot": { Deny: true },
+  // symlink to ../run which is on tmpfs
+  "/var/run": { Deny: true },
+  // symlink to ../run/lock which is on tmpfs
+  "/var/lock": { Deny: true },
+};
